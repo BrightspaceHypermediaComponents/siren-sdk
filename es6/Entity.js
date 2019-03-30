b2945ed7f38ff553e25f149437876c27033b01a6
Maya Jones <Maya.Jones@D2L.com>
2019-03-30 19:43:09 -0400

    Add JSDocs comments.

--- es6/Entity.js
+++ es6/Entity.js
@@ -1,6 +1,16 @@
 'use strict';
+import { entityFactory, dispose } from 'entityFactory.js';
 
+/**
+ * Abstract Entity class to help create entity classes.
+ */
 export class Entity {
+	/**
+	 * Primes the object used by the entityFactory. Should never be called outside.
+	 * @param {Object} entity A siren entity
+	 * @param {String|Function|null} token token
+	 * @param {Function} listener Listener helper class
+	 */
 	constructor(entity, token, listener) {
 		this._entity = entity;
 		this._subEntities = new Map();
@@ -8,18 +18,26 @@ export class Entity {
 		this._listener = listener;
 	}
 
+	/**
+	 * Cleans up this entity by deleting the callbacks listeners stored in the entity store.
+	 */
 	dispose() {
-		this._subEntities.forEach(entity => entity.dispose());
+		this._subEntities.forEach(entity => dispose(entity));
 		this._listener.remove();
 	}
 
-	// protected method to keep track of sub entities.
-	_subEntity(entityType, href, token, onChange) {
+	/**
+	 * Protected: Add a listener to a subentity of this entity.
+	 * @param {*} entityType A entity class that extends this class.
+	 * @param {*} href Href of the entity to be created
+	 * @param {*} onChange Function to be called when the subentity changes.
+	 */
+	_subEntity(entityType, href, onChange) {
 		// Clean up if that href has already been added.
 		if (this._subEntities.has(href)) {
-			this._subEntities.get(href).dispose();
+			dispose(this._subEntities.get(href));
 		}
-		entityFactory(entityType, href, token, (entity) => {
+		entityFactory(entityType, href, this._token, (entity) => {
 			this._subEntities.set(href, entity);
 			onChange(entity);
 		});
--- mixin/entity-mixin.js
+++ mixin/entity-mixin.js
@@ -1,20 +1,29 @@
 'use strict';
 import { dedupingMixin } from '@polymer/polymer/lib/utils/mixin.js';
-import { entityFactory, decompose } from '../es6/EntityFactory.js';
+import { entityFactory, dispose } from '../es6/EntityFactory.js';
 
-/*
-	This mixin takes care of all the clean up. Thanks to entity be able to clean up all it's childern.
-*/
-/* @polymerMixin */
+/**
+ * This mixin takes care of all the clean up. Thanks to entity be able to clean up all it's childern.
+ * @polymerMixin
+ */
 export const interalEntityMixin = function(superClass) {
 	return class extends superClass {
 		static get properties() {
 			return {
+				/**
+				 * Href for the entity
+				 */
 				href: {
 					type: String,
 					reflectToAttribute: true
 				},
+				/**
+				 * Token
+				 */
 				token: String,
+				/**
+				 * A real object.
+				 */
 				_entity: Object
 			};
 		}
@@ -26,13 +35,16 @@ export const interalEntityMixin = function(superClass) {
 		}
 
 		detached() {
-			// this calls this._entity.decompose() if entity is actually an entity.
-			// Note this will decompose all child entities used.
-			decompose(this._entity);
+			// this calls this._entity.dispose() if entity is actually an entity.
+			// Note this will dispose all child entities used.
+			dispose(this._entity);
 		}
 
-		// Protected method to set the entity type such as Organization Entity.
-		// Requires to be called in the constructor
+		/**
+		 * Protected method to set the entity type such as Organization Entity.
+		 * Requires to be called in the constructor
+		 * @param {Function} entityType  The type of the entity. For example OrganizationEntity
+		 */
 		_setEntityType(entityType) {
 			if (typeof entityType === 'function') {
 				this._entityType = entityType;
@@ -42,7 +54,7 @@ export const interalEntityMixin = function(superClass) {
 		__onHrefChange(href, token) {
 			if (typeof this._entityType === 'function') {
 				entityFactory(this._entityType, href, token, entity => {
-					decompose(this._entity); // Make sure the entity is cleaned up before setting a new one.
+					dispose(this._entity); // Make sure the entity is cleaned up before setting a new one.
 					this._entity = entity;
 				});
 			}
