d49fca9d677e29d97ba9fd325293c75a9957e8da
Edward Gao <Edward.Gao@D2L.com>
2019-04-12 17:04:29 -0400

    Add SimpleEntity and siren behavior dependency

---
+++ es6/SimpleEntity.js
@@ -0,0 +1,11 @@
+'use strict';
+import { Entity } from './Entity.js';
+
+/**
+ * Generic Entity class that only returns an entity itself
+ */
+export class SimpleEntity extends Entity {
+	entity() {
+		return this._entity;
+	}
+}
