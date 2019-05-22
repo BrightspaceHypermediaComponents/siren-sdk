764b8d479e519bae437b84d162b8d1a61bb75e29
Cheick Abba Cisse <cheick95@hotmail.com>
2019-05-22 12:17:56 -0400

    US107060 Lint Errors

--- src/userSettings/UserSettingsEntity.js
+++ src/userSettings/UserSettingsEntity.js
@@ -10,24 +10,24 @@ export class UserSettingsEntity extends Entity {
 			return;
 		}
 		return this._entity.getLinkByRel(Rels.widgetSettings).href;
-    }
+	}
 
-    userSettingsAction() {
-        if (!this._entity || !this._entity.hasActionByName(Actions.enrollments.updateUserSettings)) {
-            return;
-        }
+	userSettingsAction() {
+		if (!this._entity || !this._entity.hasActionByName(Actions.enrollments.updateUserSettings)) {
+			return;
+		}
 		return this._entity.getActionByName(Actions.enrollments.updateUserSettings);
-    }
+	}
 
-    mostRecentEnrollmentsSearchName() {
-        return this._entity
+	mostRecentEnrollmentsSearchName() {
+		return this._entity
             && this._entity.properties
             && this._entity.properties.MostRecentEnrollmentsSearchName;
-    }
+	}
 
-    mostRecentEnrollmentsSearchType() {
-        return this._entity
+	mostRecentEnrollmentsSearchType() {
+		return this._entity
             && this._entity.properties
             && this._entity.properties.MostRecentEnrollmentsSearchType;
-    }
+	}
 }
