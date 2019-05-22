a3e92399e4dcc0b3ee3465b7ac79b474a2b5e15a
Cheick Abba Cisse <cheick95@hotmail.com>
2019-05-22 17:22:40 -0400

    US107060 Fixed lint UserSettingEntity

--- test/UserSettingsEntity/UserSettingsEntity.js
+++ test/UserSettingsEntity/UserSettingsEntity.js
@@ -2,84 +2,83 @@
 import { UserSettingsEntity } from '../../src/userSettings/UserSettingsEntity';
 
 describe('UserSettingsEntity', () => {
-    var entity, action;
+	var entity, action;
 
-    beforeEach(() => {
-        entity = window.D2L.Hypermedia.Siren.Parse({
-            "properties": {
-                "MostRecentEnrollmentsSearchType": "None",
-                "MostRecentEnrollmentsSearchName": "search-my-enrollments"
-            },
-            "rel": [
-                "https://api.brightspace.com/rels/user-settings"
-            ],
-            "links": [
-                {
-                    "rel": [
-                        "https://api.brightspace.com/rels/widget-settings"
-                    ],
-                    "href": "../data/presentation"
-                },
-                {
-                    "rel": [
-                        "self"
-                    ],
-                    "href": "../data/user-settings"
-                }
-            ],
-            "actions": [
-                {
-                    "href": "../data/user-settings",
-                    "name": "update-user-settings",
-                    "method": "PUT"
-                }
-            ]
-        });
-    })
+	beforeEach(() => {
+		entity = window.D2L.Hypermedia.Siren.Parse({
+			'properties': {
+				'MostRecentEnrollmentsSearchType': 'None',
+				'MostRecentEnrollmentsSearchName': 'search-my-enrollments'
+			},
+			'rel': [
+				'https://api.brightspace.com/rels/user-settings'
+			],
+			'links': [
+				{
+					'rel': [
+						'https://api.brightspace.com/rels/widget-settings'
+					],
+					'href': '../data/presentation'
+				},
+				{
+					'rel': [
+						'self'
+					],
+					'href': '../data/user-settings'
+				}
+			],
+			'actions': [
+				{
+					'href': '../data/user-settings',
+					'name': 'update-user-settings',
+					'method': 'PUT'
+				}
+			]
+		});
+	});
 
 	describe('Tests for userSettingsHref', () => {
 		it('Return correct href', () => {
-            var userSettings = new UserSettingsEntity(entity);
-            console.log(userSettings);
+			var userSettings = new UserSettingsEntity(entity);
 			expect(userSettings.userSettingsHref()).to.equal('../data/presentation');
-        });
-    });
+		});
+	});
 
-    describe('Tests for userSettingsAction', () => {
-        beforeEach(() => {
-            action = {
-                "href": "../data/user-settings",
-                "name": "update-user-settings",
-                "method": "PUT"
-            };
-        });
+	describe('Tests for userSettingsAction', () => {
+		beforeEach(() => {
+			action = {
+				'href': '../data/user-settings',
+				'name': 'update-user-settings',
+				'method': 'PUT'
+			};
+		});
 
 		it('Return correct Action href', () => {
-            var userSettings = new UserSettingsEntity(entity);
-			expect(userSettings.userSettingsAction().href).to.equal(action.href)
-        });
+			var userSettings = new UserSettingsEntity(entity);
+			expect(userSettings.userSettingsAction().href).to.equal(action.href);
+		});
 
-        it('Return correct Action name', () => {
-            var userSettings = new UserSettingsEntity(entity);
-			expect(userSettings.userSettingsAction().name).to.equal(action.name)
-        });
+		it('Return correct Action name', () => {
+			var userSettings = new UserSettingsEntity(entity);
+			expect(userSettings.userSettingsAction().name).to.equal(action.name);
+		});
+		
+		it('Return correct Action method', () => {
+			var userSettings = new UserSettingsEntity(entity);
+			expect(userSettings.userSettingsAction().method).to.equal(action.method);
+		});
+	});
 
-        it('Return correct Action method', () => {
-            var userSettings = new UserSettingsEntity(entity);
-			expect(userSettings.userSettingsAction().method).to.equal(action.method)
-        });
-    });
-
-    describe('Tests for Propreties', () => {
+	describe('Tests for Propreties', () => {
 		it('Return correct mostRecentEnrollmentsSearchName', () => {
-            var userSettings = new UserSettingsEntity(entity);
+			var userSettings = new UserSettingsEntity(entity);
 			expect(userSettings.mostRecentEnrollmentsSearchName()).to.equal('search-my-enrollments');
-        });
-
-        it('Return correct mostRecentEnrollmentsSearchType', () => {
-            var userSettings = new UserSettingsEntity(entity);
+		});
+		
+		it('Return correct mostRecentEnrollmentsSearchType', () => {
+			var userSettings = new UserSettingsEntity(entity);
 			expect(userSettings.mostRecentEnrollmentsSearchType()).to.equal('None');
-        });
+		});
 	});
 
 });
