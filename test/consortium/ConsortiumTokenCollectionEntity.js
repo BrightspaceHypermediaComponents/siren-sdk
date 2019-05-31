689b0741b10c89a8d46be4320d0efd1352e2374b
Chester Twomey <Chester.Twomey@D2L.com>
2019-05-30 20:28:57 -0500

    test fix

--- test/consortium/ConsortiumTokenCollectionEntity.js
+++ test/consortium/ConsortiumTokenCollectionEntity.js
@@ -3,93 +3,111 @@ import { ConsortiumTokenCollectionEntity } from '../../src/consortium/Consortium
 import { ConsortiumTokenEntity } from '../../src/consortium/ConsortiumTokenEntity.js';
 import { Rels } from '../../src/hypermedia-constants.js';
 import { root } from '../../src/root/root.js';
+import rootResponse from './root.json';
 
 describe('Consortium entity', () => {
+	var sandbox;
 
-	describe('Consortium entity', () => {
-		const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidGVuYW50aWQiOiIxY2IxNmQ2YS04NTU3LTQ4NTAtODg0Ni0zZmE5YjYxNzQ0OTQiLCJpYXQiOjE1MTYyMzkwMjJ9.z5hMu02Cx8p7Lw0a_1nBVTkMD2UncN5UZre3l0SWo7c';
-		const tenant1 = '1cb16d6a-8557-4850-8846-3fa9b6174494';
-		const tenant2 = '8b33e567-c616-4667-868b-fdfe9edc3a78';
-		const token2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidGVuYW50aWQiOiI4YjMzZTU2Ny1jNjE2LTQ2NjctODY4Yi1mZGZlOWVkYzNhNzgiLCJpYXQiOjE1MTYyMzkwMjJ9.cQjR28qT_c-os_FeFy4-L1NhCIY-9utPLzSHrzIMuOc';
-		it('has collection', async() => {
-			const entity =  window.D2L.Hypermedia.Siren.Parse({
-				class: ['tokens'],
-				entities: [
-					{
-						class: [
-							'token',
-						],
-						rel: [],
-						properties: {
-							tenant: tenant1,
-							token: token1
-						},
-						links: [
-							{
-								rel: ['self'],
-								href: 'https://consortium.api.dev.brightspace.com/1cb16d6a-8557-4850-8846-3fa9b6174494'
-							},
-							{
-								rel: [Rels.root],
-								href: '/root.json'
-							}
-						]
-					},
-					{
-						class: [
-							'token',
-						],
-						rel: [],
-						properties: {
-							tenant: tenant2,
-							token: token2
-						},
-						links: [
-							{
-								rel: ['self'],
-								href: 'https://consortium.api.dev.brightspace.com/8b33e567-c616-4667-868b-fdfe9edc3a78'
-							},
-							{
-								rel: [Rels.root],
-								href: '/root.json'
-							}
-						]
-					}
-				]
-			});
-			const consortium = new ConsortiumTokenCollectionEntity(entity);
-			expect(consortium._consortiumTokenEntities().length).to.equal(2);
-			const tokenEntities = [];
-			const rootEntities = [];
-			consortium.consortiumTokenEntities((entity) => {
-				tokenEntities.push(entity);
-				entity.rootOrganizationEntity((root) =>  {
-					rootEntities.push(root);
-				});
-			});
-			await new Promise(async(resolve) => {
-				for (;;) {
-					if (tokenEntities.length >= 2 && rootEntities.length >= 2) {
-						resolve();
-					}
-					await new Promise(resolve => {
-						setTimeout(()=>{ resolve();}, 5);
-					});
-				}
-			});
+	beforeEach(() => {
+		sandbox = sinon.sandbox.create();
 
-			expect(tokenEntities.length, 'token entities length invalid').to.be.equal(2);
-			for (const entity of tokenEntities) {
-				expect(entity).to.be.an.instanceof(ConsortiumTokenEntity);
-			}
-			expect(rootEntities.length, 'root entities length invalid').to.be.equal(2);
-			for (const rootEntity of rootEntities) {
-				expect(rootEntity).to.be.an.instanceof(root);
-			}
-			expect(tokenEntities[0].consortiumToken()).to.be.equal(token1);
-			expect(tokenEntities[1].consortiumToken()).to.be.equal(token2);
-			expect(tokenEntities[0].consortiumTenant()).to.be.equal(tenant1);
-			expect(tokenEntities[1].consortiumTenant()).to.be.equal(tenant2);
+		sandbox.stub(window.d2lfetch, 'fetch', (input) => {
+			const whatToFetch = {
+				'/root.json': rootResponse
+			};
+			return Promise.resolve({
+				ok: true,
+				json: () => { return Promise.resolve(whatToFetch[input]); }
+			});
 		});
 	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+	const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidGVuYW50aWQiOiIxY2IxNmQ2YS04NTU3LTQ4NTAtODg0Ni0zZmE5YjYxNzQ0OTQiLCJpYXQiOjE1MTYyMzkwMjJ9.z5hMu02Cx8p7Lw0a_1nBVTkMD2UncN5UZre3l0SWo7c';
+	const tenant1 = '1cb16d6a-8557-4850-8846-3fa9b6174494';
+	const tenant2 = '8b33e567-c616-4667-868b-fdfe9edc3a78';
+	const token2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidGVuYW50aWQiOiI4YjMzZTU2Ny1jNjE2LTQ2NjctODY4Yi1mZGZlOWVkYzNhNzgiLCJpYXQiOjE1MTYyMzkwMjJ9.cQjR28qT_c-os_FeFy4-L1NhCIY-9utPLzSHrzIMuOc';
+	it('has collection', async() => {
+		const entity =  window.D2L.Hypermedia.Siren.Parse({
+			class: ['tokens'],
+			entities: [
+				{
+					class: [
+						'token',
+					],
+					rel: [],
+					properties: {
+						tenant: tenant1,
+						token: token1
+					},
+					links: [
+						{
+							rel: ['self'],
+							href: 'https://consortium.api.dev.brightspace.com/1cb16d6a-8557-4850-8846-3fa9b6174494'
+						},
+						{
+							rel: [Rels.root],
+							href: '/root.json'
+						}
+					]
+				},
+				{
+					class: [
+						'token',
+					],
+					rel: [],
+					properties: {
+						tenant: tenant2,
+						token: token2
+					},
+					links: [
+						{
+							rel: ['self'],
+							href: 'https://consortium.api.dev.brightspace.com/8b33e567-c616-4667-868b-fdfe9edc3a78'
+						},
+						{
+							rel: [Rels.root],
+							href: '/root.json'
+						}
+					]
+				}
+			]
+		});
+		const consortium = new ConsortiumTokenCollectionEntity(entity);
+		expect(consortium._consortiumTokenEntities().length).to.equal(2);
+		const tokenEntities = [];
+		const rootEntities = [];
+		consortium.consortiumTokenEntities((entity) => {
+			tokenEntities.push(entity);
+			entity.rootOrganizationEntity((root) =>  {
+				rootEntities.push(root);
+			});
+		});
+		await new Promise(async(resolve) => {
+			for (;;) {
+				if (tokenEntities.length >= 2 && rootEntities.length >= 2) {
+					resolve();
+				}
+				await new Promise(resolve => {
+					setTimeout(()=>{ resolve();}, 5);
+				});
+			}
+		});
+
+		expect(tokenEntities.length, 'token entities length invalid').to.be.equal(2);
+		for (const entity of tokenEntities) {
+			expect(entity).to.be.an.instanceof(ConsortiumTokenEntity);
+		}
+		expect(rootEntities.length, 'root entities length invalid').to.be.equal(2);
+		for (const rootEntity of rootEntities) {
+			expect(rootEntity).to.be.an.instanceof(root);
+		}
+		expect(tokenEntities[0].consortiumToken()).to.be.equal(token1);
+		expect(tokenEntities[1].consortiumToken()).to.be.equal(token2);
+		expect(tokenEntities[0].consortiumTenant()).to.be.equal(tenant1);
+		expect(tokenEntities[1].consortiumTenant()).to.be.equal(tenant2);
+	});
 });
+
