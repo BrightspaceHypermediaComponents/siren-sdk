1f963e0b14a5f703ed5b099241f36c408ff25d4c
Chester Twomey <Chester.Twomey@D2L.com>
2019-06-04 10:42:16 -0500

    add organization entity reference to root, add homepage link

--- src/root/root.js
+++ src/root/root.js
@@ -2,6 +2,7 @@ import { EnrollmentCollectionEntity } from '../enrollments/EnrollmentCollectionE
 import { Entity } from '../es6/Entity.js';
 import { entityFactory } from '../es6/EntityFactory.js';
 import { Rels } from '../hypermedia-constants.js';
+import { OrganizationEntity } from '../organizations/OrganizationEntity.js';
 
 export class root extends Entity {
 	/**
@@ -9,10 +10,10 @@ export class root extends Entity {
 	 * @param function onChange Callback function that accepts an {function} to be called when entity changes
 	 */
 	enrollments(onChange) {
-		if (!this._enrollmentshref) {
+		if (!this._enrollmentshref()) {
 			return;
 		}
-		entityFactory(EnrollmentCollectionEntity, this._enrollmentshref, this._token, onChange);
+		entityFactory(EnrollmentCollectionEntity, this._enrollmentshref(), this._token, onChange);
 	}
 
 	_enrollmentshref() {
@@ -23,4 +24,21 @@ export class root extends Entity {
 		return this._entity.getLinkByRel(Rels.enrollments).href;
 	}
 
+	/**
+	 * A constructed {@link OrganizationEntity}
+	 */
+	organization(onChange) {
+		if (!this._organizationHref()) {
+			return;
+		}
+		entityFactory(OrganizationEntity, this._organizationHref(), this._token, onChange);
+	}
+
+	_organizationHref() {
+		if (!this._entity || !this._entity.hasLinkByRel(Rels.organization)) {
+			return;
+		}
+
+		return this._entity.getLinkByRel(Rels.organization).href;
+	}
 }
