62d8132f07125cdcd1f7e80103daa9215ce79e9c
Cheick Cisse <Cheick.Cisse@D2L.com>
2019-06-05 13:52:48 -0400

    PresentationEntity

---
+++ src/presentation/PresentationEntity.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import { Entity } from '../es6/Entity.js';
+import { Rels, Actions } from '../hypermedia-constants';
+/**
+ * A collection of sub entities pointing to distinct enrollments
+ */
+export class PresentationEntity extends Entity {
+	/**
+	 * @returns list of string represented hrefs pointing to siren entities
+	 */
+	hideCourseStartDate() {
+		return this._entity && this._entity.properties && this._entity.properties.HideCourseStartDate;
+	}
+
+	hideCourseEndDate() {
+		return this._entity && this._entity.properties && this._entity.properties.HideCourseEndDate;
+	}
+
+	showOrganizationCode() {
+		return this._entity && this._entity.properties && this._entity.propertiess.ShowCourseCode;
+	}
+
+	showSemesterName() {
+		return this._entity && this._entity.properties && this._entity.properties.ShowSemester;
+	}
+
+	showDropboxUnreadFeedback() {
+		return this._entity && this._entity.properties && this._entity.properties.ShowDropboxUnreadFeedback
+	}
+
+	showUnattemptedQuizzes() {
+		return this._entity && this._entity.properties && this._entity.properties.ShowUnattemptedQuizzes;
+	}
+
+	showUngradedQuizAttempts() {
+		return this._entity && this._entity.properties && this._entity.properties.ShowUngradedQuizAttempts;
+    }
+    showUnreadDiscussionMessages() {
+		return this._entity && this._entity.properties && this._entity.properties.ShowUnreadDiscussionMessages;
+    }
+    showUnreadDropboxSubmissions() {
+		return this._entity && this._entity.properties && this._entity.properties.ShowUnreadDropboxSubmissions;
+	}
+}
