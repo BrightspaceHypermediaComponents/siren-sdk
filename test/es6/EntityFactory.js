c9e4373198b26c447a379e1520e9d9d764675023
Chester Twomey <chester.twomey@d2l.com>
2019-09-04 15:34:41 -0500

    propegate error values from siren store (#60)
    * propegate error values from siren store
    
    * fix test
    
    * stub out fetch and force a 404

---
+++ test/es6/EntityFactory.js
@@ -0,0 +1,38 @@
+import { entityFactory } from '../../src/es6/EntityFactory.js';
+import { Nothing } from '../nothing-entity.js';
+
+/* global describe it expect sinon*/
+window.D2L.Siren.WhitelistBehavior._testMode(true);
+describe('Entityfactory', () => {
+
+	describe('errors', () => {
+		var sandbox;
+
+		beforeEach(() => {
+			sandbox = sinon.sandbox.create();
+
+			sandbox.stub(window.d2lfetch, 'fetch', () => {
+
+				return Promise.resolve({
+					ok: false,
+					status: 404
+				}
+				);
+			});
+		});
+
+		afterEach(() => {
+			sandbox.restore();
+		});
+		it('bubbles to onchange', done => {
+			entityFactory(Nothing, 'http://localhost/not/a/valid/thing', 'wutsatoken', (entity, err) => {
+				expect(entity).to.be.null;
+				expect(err).to.be.not.null;
+				expect(err).to.be.equal(404);
+				done();
+			});
+
+		});
+	});
+});
+
