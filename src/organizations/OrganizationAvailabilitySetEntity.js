23e077d4061ffe9073c3020613bf29e6f00eb3ae
Vincent Wong <Vincent.Wong@D2L.com>
2019-10-28 14:27:03 -0700

    Update siren-sdk to use "Current" class to identify the current org unit

--- src/organizations/OrganizationAvailabilitySetEntity.js
+++ src/organizations/OrganizationAvailabilitySetEntity.js
@@ -8,25 +8,16 @@ import { OrganizationEntity } from './OrganizationEntity.js';
  * OrganizationAvailabilitySetEntity class representation of a d2l OrgUnitAvailabilitySet.
  */
 export class OrganizationAvailabilitySetEntity extends Entity {
-	_getCurrentOrgUnitAvailabilityHref() {
-		const currentOrganizationHref = this.getOrganizationHref();
-		const currentOrgUnitId = currentOrganizationHref.substr(currentOrganizationHref.lastIndexOf('/') + 1);
-		const selfHref = this._entity.getLinkByRel('self').href;
-		return selfHref + '/explicit/' + currentOrgUnitId;
+	getCurrentOrgUnitEntity() {
+		return this._entity.getSubEntityByClass(Classes.organizationAvailability.current);
 	}
 
-	getCurrentOrgUnitAvailability() {
-		const currentOrgUnitAvailabilityHref = this._getCurrentOrgUnitAvailabilityHref();
-		return this.getAvailabilityEntities().find(e => e.href === currentOrgUnitAvailabilityHref);
-	}
-
-	getAvailabilityEntities() {
+	_getAvailabilityEntities() {
 		return this._entity.getSubEntitiesByClass(Classes.organizationAvailability.orgUnitAvailability);
 	}
 
-	getAvailabilityEntitiesWithoutCurrentOrgUnit() {
-		const currentOrgUnitAvailabilityHref = this._getCurrentOrgUnitAvailabilityHref();
-		return this.getAvailabilityEntities().filter(e => e.href !== currentOrgUnitAvailabilityHref);
+	getEntitiesExcludingCurrentOrgUnit() {
+		return this._getAvailabilityEntities().filter(e => !e.hasClass(Classes.organizationAvailability.current));
 	}
 
 	getOrganizationHref() {
