46d65d7c29ec1753394343eb01ea120a3ddf5c0c
Margaret Dulat <Margaret.Dulat@D2L.com>
2020-01-02 14:18:57 -0800

    US111489 create sandbox for testing only once

--- test/activities/ActivityUsageEntity.js
+++ test/activities/ActivityUsageEntity.js
@@ -34,224 +34,206 @@ describe('ActivityUsageEntity', () => {
 		});
 	});
 
-	describe('Due Date', () => {
-		describe('Can edit', () => {
-			let sandbox, setDueDateSpy;
+	describe('Functionality', () => {
+		let sandbox;
 
-			beforeEach(() => {
-				sandbox = sinon.sandbox.create();
-				setDueDateSpy = sandbox.spy(entity, 'setDueDate');
+		beforeEach(() => {
+			sandbox = sinon.sandbox.create();
+		});
+
+		afterEach(() => {
+			sandbox.restore();
+		});
+
+		describe('Due Date', () => {
+			describe('Can edit', () => {
+				let setDueDateSpy;
+
+				beforeEach(() => {
+					setDueDateSpy = sandbox.spy(entity, 'setDueDate');
+				});
+
+				it('gets due date', () => {
+					expect(entity.dueDate()).to.equal('2019-12-26T04:59:00.000Z');
+				});
+
+				it('can edit due date', () => {
+					entity.canEditDueDate().then(result => expect(result).to.be.true);
+				});
+
+				it('returns a promise when setting due date', () => {
+					entity.setDueDate('2019-12-27T04:59:00.000Z');
+					expect(setDueDateSpy.returnValues[0]).to.be.a('promise');
+				});
 			});
 
-			afterEach(() => {
-				sandbox.restore();
-			});
+			describe('Can NOT edit', () => {
+				let setDueDateSpy;
 
-			it('gets due date', () => {
-				expect(entity.dueDate()).to.equal('2019-12-26T04:59:00.000Z');
-			});
+				beforeEach(() => {
+					setDueDateSpy = sandbox.spy(readonlyEntity, 'setDueDate');
+				});
 
-			it('can edit due date', () => {
-				entity.canEditDueDate().then(result => expect(result).to.be.true);
-			});
+				it('gets due date', () => {
+					expect(readonlyEntity.dueDate()).to.equal('2019-12-26T04:59:00.000Z');
+				});
 
-			it('returns a promise when setting due date', () => {
-				entity.setDueDate('2019-12-27T04:59:00.000Z');
-				expect(setDueDateSpy.returnValues[0]).to.be.a('promise');
+				it('returns false for canEditDueDate function', () => {
+					readonlyEntity.canEditDueDate().then(result => expect(result).to.be.false);
+				});
+
+				it('returns undefined if attempting to edit due date', () => {
+					readonlyEntity.setDueDate('2019-12-27T04:59:00.000Z')
+						.then(() => expect(setDueDateSpy.returnValues[0]).to.be.undefined);
+				});
 			});
 		});
 
-		describe('Can NOT edit', () => {
-			let sandbox, setDueDateSpy;
+		describe('Draft Status', () => {
+			describe('Can edit', () => {
+				let setDraftStatusSpy;
 
-			beforeEach(() => {
-				sandbox = sinon.sandbox.create();
-				setDueDateSpy = sandbox.spy(readonlyEntity, 'setDueDate');
+				beforeEach(() => {
+					setDraftStatusSpy = sandbox.spy(entity, 'setDraftStatus');
+				});
+
+				it('gets isDraft', () => {
+					expect(entity.isDraft()).to.be.true;
+				});
+
+				it('gets isPublished', () => {
+					expect(entity.isPublished()).to.be.false;
+				});
+
+				it('can edit draft', () => {
+					expect(entity.canEditDraft()).to.be.true;
+				});
+
+				it('returns a promise when setting draft', () => {
+					entity.setDraftStatus(false);
+					expect(setDraftStatusSpy.returnValues[0]).to.be.a('promise');
+				});
 			});
 
-			afterEach(() => {
-				sandbox.restore();
-			});
+			describe('Can NOT edit', () => {
+				let setDraftStatusSpy;
 
-			it('gets due date', () => {
-				expect(readonlyEntity.dueDate()).to.equal('2019-12-26T04:59:00.000Z');
-			});
+				beforeEach(() => {
+					setDraftStatusSpy = sandbox.spy(readonlyEntity, 'setDraftStatus');
+				});
 
-			it('returns false for canEditDueDate function', () => {
-				readonlyEntity.canEditDueDate().then(result => expect(result).to.be.false);
-			});
+				it('gets isDraft', () => {
+					expect(readonlyEntity.isDraft()).to.be.true;
+				});
 
-			it('returns undefined if attempting to edit due date', () => {
-				readonlyEntity.setDueDate('2019-12-27T04:59:00.000Z')
-					.then(() => expect(setDueDateSpy.returnValues[0]).to.be.undefined);
-			});
-		});
-	});
+				it('gets isPublished', () => {
+					expect(readonlyEntity.isPublished()).to.be.false;
+				});
 
-	describe('Draft Status', () => {
-		describe('Can edit', () => {
-			let sandbox, setDraftStatusSpy;
+				it('can edit draft', () => {
+					expect(readonlyEntity.canEditDraft()).to.be.false;
+				});
 
-			beforeEach(() => {
-				sandbox = sinon.sandbox.create();
-				setDraftStatusSpy = sandbox.spy(entity, 'setDraftStatus');
-			});
-
-			afterEach(() => {
-				sandbox.restore();
-			});
-
-			it('gets isDraft', () => {
-				expect(entity.isDraft()).to.be.true;
-			});
-
-			it('gets isPublished', () => {
-				expect(entity.isPublished()).to.be.false;
-			});
-
-			it('can edit draft', () => {
-				expect(entity.canEditDraft()).to.be.true;
-			});
-
-			it('returns a promise when setting draft', () => {
-				entity.setDraftStatus(false);
-				expect(setDraftStatusSpy.returnValues[0]).to.be.a('promise');
+				it('returns undefined if attempting to edit draft state', () => {
+					readonlyEntity.setDraftStatus(false)
+						.then(() => expect(setDraftStatusSpy.returnValues[0]).to.be.undefined);
+				});
 			});
 		});
 
-		describe('Can NOT edit', () => {
-			let sandbox, setDraftStatusSpy;
+		describe('Score Out Of', () => {
+			describe('Can edit', () => {
+				let setScoreOutOfSpy, removeFromGradesSpy, addToGradesSpy, setUngradedSpy;
 
-			beforeEach(() => {
-				sandbox = sinon.sandbox.create();
-				setDraftStatusSpy = sandbox.spy(readonlyEntity, 'setDraftStatus');
+				beforeEach(() => {
+					setScoreOutOfSpy = sandbox.spy(entity, 'setScoreOutOf');
+					removeFromGradesSpy = sandbox.spy(entity, 'removeFromGrades');
+					addToGradesSpy = sandbox.spy(entity, 'addToGrades');
+					setUngradedSpy = sandbox.spy(entity, 'setUngraded');
+				});
+
+				it('gets scoreOutOf', () => {
+					expect(entity.scoreOutOf()).to.equal(56);
+				});
+
+				it('gets inGrades', () => {
+					expect(entity.inGrades()).to.equal(true);
+				});
+
+				it('gets gradeType', () => {
+					expect(entity.gradeType()).to.equal('Points');
+				});
+
+				it('can edit score out of', () => {
+					expect(entity.canEditScoreOutOf()).to.be.true;
+				});
+
+				it('returns a promise when setting scoreOutOf', () => {
+					entity.setScoreOutOf('70');
+					expect(setScoreOutOfSpy.returnValues[0]).to.be.a('promise');
+				});
+
+				it('returns a promise when removing from grades', () => {
+					entity.removeFromGrades();
+					expect(removeFromGradesSpy.returnValues[0]).to.be.a('promise');
+				});
+
+				it('returns a promise when adding to grades', () => {
+					entity.addToGrades();
+					expect(addToGradesSpy.returnValues[0]).to.be.a('promise');
+				});
+
+				it('returns a promise when setting ungraded', () => {
+					entity.setUngraded();
+					expect(setUngradedSpy.returnValues[0]).to.be.a('promise');
+				});
 			});
 
-			afterEach(() => {
-				sandbox.restore();
-			});
+			describe('Can NOT edit', () => {
+				let setScoreOutOfSpy, removeFromGradesSpy, addToGradesSpy, setUngradedSpy;
 
-			it('gets isDraft', () => {
-				expect(readonlyEntity.isDraft()).to.be.true;
-			});
+				beforeEach(() => {
+					setScoreOutOfSpy = sandbox.spy(readonlyEntity, 'setScoreOutOf');
+					removeFromGradesSpy = sandbox.spy(readonlyEntity, 'removeFromGrades');
+					addToGradesSpy = sandbox.spy(readonlyEntity, 'addToGrades');
+					setUngradedSpy = sandbox.spy(readonlyEntity, 'setUngraded');
+				});
 
-			it('gets isPublished', () => {
-				expect(readonlyEntity.isPublished()).to.be.false;
-			});
+				it('gets scoreOutOf', () => {
+					expect(readonlyEntity.scoreOutOf()).to.equal(56);
+				});
 
-			it('can edit draft', () => {
-				expect(readonlyEntity.canEditDraft()).to.be.false;
-			});
+				it('gets inGrades', () => {
+					expect(readonlyEntity.inGrades()).to.equal(true);
+				});
 
-			it('returns undefined if attempting to edit draft state', () => {
-				readonlyEntity.setDraftStatus(false)
-					.then(() => expect(setDraftStatusSpy.returnValues[0]).to.be.undefined);
-			});
-		});
-	});
+				it('gets gradeType', () => {
+					expect(readonlyEntity.gradeType()).to.equal('Points');
+				});
 
-	describe('Score Out Of', () => {
-		describe('Can edit', () => {
-			let sandbox, setScoreOutOfSpy, removeFromGradesSpy, addToGradesSpy, setUngradedSpy;
+				it('can edit score out of', () => {
+					expect(readonlyEntity.canEditScoreOutOf()).to.be.false;
+				});
 
-			beforeEach(() => {
-				sandbox = sinon.sandbox.create();
-				setScoreOutOfSpy = sandbox.spy(entity, 'setScoreOutOf');
-				removeFromGradesSpy = sandbox.spy(entity, 'removeFromGrades');
-				addToGradesSpy = sandbox.spy(entity, 'addToGrades');
-				setUngradedSpy = sandbox.spy(entity, 'setUngraded');
-			});
+				it('returns undefined if attempting to setting scoreOutOf', () => {
+					readonlyEntity.setScoreOutOf(70)
+						.then(() => expect(setScoreOutOfSpy.returnValues[0]).to.be.undefined);
+				});
 
-			afterEach(() => {
-				sandbox.restore();
-			});
+				it('returns undefined if attempting to removing from grades', () => {
+					readonlyEntity.removeFromGrades()
+						.then(() => expect(removeFromGradesSpy.returnValues[0]).to.be.undefined);
+				});
 
-			it('gets scoreOutOf', () => {
-				expect(entity.scoreOutOf()).to.equal(56);
-			});
+				it('returns undefined if attempting to adding to grades', () => {
+					readonlyEntity.addToGrades()
+						.then(() => expect(addToGradesSpy.returnValues[0]).to.be.undefined);
+				});
 
-			it('gets inGrades', () => {
-				expect(entity.inGrades()).to.equal(true);
-			});
-
-			it('gets gradeType', () => {
-				expect(entity.gradeType()).to.equal('Points');
-			});
-
-			it('can edit score out of', () => {
-				expect(entity.canEditScoreOutOf()).to.be.true;
-			});
-
-			it('returns a promise when setting scoreOutOf', () => {
-				entity.setScoreOutOf('70');
-				expect(setScoreOutOfSpy.returnValues[0]).to.be.a('promise');
-			});
-
-			it('returns a promise when removing from grades', () => {
-				entity.removeFromGrades();
-				expect(removeFromGradesSpy.returnValues[0]).to.be.a('promise');
-			});
-
-			it('returns a promise when adding to grades', () => {
-				entity.addToGrades();
-				expect(addToGradesSpy.returnValues[0]).to.be.a('promise');
-			});
-
-			it('returns a promise when setting ungraded', () => {
-				entity.setUngraded();
-				expect(setUngradedSpy.returnValues[0]).to.be.a('promise');
-			});
-		});
-
-		describe('Can NOT edit', () => {
-			let sandbox, setScoreOutOfSpy, removeFromGradesSpy, addToGradesSpy, setUngradedSpy;
-
-			beforeEach(() => {
-				sandbox = sinon.sandbox.create();
-				setScoreOutOfSpy = sandbox.spy(readonlyEntity, 'setScoreOutOf');
-				removeFromGradesSpy = sandbox.spy(readonlyEntity, 'removeFromGrades');
-				addToGradesSpy = sandbox.spy(readonlyEntity, 'addToGrades');
-				setUngradedSpy = sandbox.spy(readonlyEntity, 'setUngraded');
-			});
-
-			afterEach(() => {
-				sandbox.restore();
-			});
-
-			it('gets scoreOutOf', () => {
-				expect(readonlyEntity.scoreOutOf()).to.equal(56);
-			});
-
-			it('gets inGrades', () => {
-				expect(readonlyEntity.inGrades()).to.equal(true);
-			});
-
-			it('gets gradeType', () => {
-				expect(readonlyEntity.gradeType()).to.equal('Points');
-			});
-
-			it('can edit score out of', () => {
-				expect(readonlyEntity.canEditScoreOutOf()).to.be.false;
-			});
-
-			it('returns undefined if attempting to setting scoreOutOf', () => {
-				readonlyEntity.setScoreOutOf(70)
-					.then(() => expect(setScoreOutOfSpy.returnValues[0]).to.be.undefined);
-			});
-
-			it('returns undefined if attempting to removing from grades', () => {
-				readonlyEntity.removeFromGrades()
-					.then(() => expect(removeFromGradesSpy.returnValues[0]).to.be.undefined);
-			});
-
-			it('returns undefined if attempting to adding to grades', () => {
-				readonlyEntity.addToGrades()
-					.then(() => expect(addToGradesSpy.returnValues[0]).to.be.undefined);
-			});
-
-			it('returns undefined if attempting to setting ungraded', () => {
-				readonlyEntity.setUngraded()
-					.then(() => expect(setUngradedSpy.returnValues[0]).to.be.undefined);
+				it('returns undefined if attempting to setting ungraded', () => {
+					readonlyEntity.setUngraded()
+						.then(() => expect(setUngradedSpy.returnValues[0]).to.be.undefined);
+				});
 			});
 		});
 	});
