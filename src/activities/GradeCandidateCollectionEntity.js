a0e04e46fb5b7b2fae074893e5a49663869b2134
Margaret Dulat <Margaret.Dulat@D2L.com>
2020-01-16 13:38:28 -0800

    US111000 return GradeCandidateEntity objects

--- src/activities/GradeCandidateCollectionEntity.js
+++ src/activities/GradeCandidateCollectionEntity.js
@@ -1,5 +1,6 @@
 'use strict';
 import { Entity } from '../es6/Entity';
+import { GradeCandidateEntity } from './GradeCandidateEntity';
 
 /**
  * Entity representation of a collection of grade candidates
@@ -9,6 +10,6 @@ export class GradeCandidateCollectionEntity extends Entity {
 	 * @returns {Array} Returns all grade-candidate sub-entities from the grade-candidates collection
 	 */
 	getGradeCandidateEntities() {
-		return this._entity.getSubEntitiesByRel('item');
+		return this._entity.getSubEntitiesByRel('item').map(entity => new GradeCandidateEntity(entity));
 	}
 }
