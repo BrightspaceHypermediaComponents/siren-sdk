16b45fdeb3818acea409e5da31c83714f837abf8
Kieran Anderson <Kieran.Anderson@D2L.com>
2020-02-25 16:55:42 -0500

    Add delete association

--- src/activities/Association.js
+++ src/activities/Association.js
@@ -1,5 +1,6 @@
 import { Entity } from '../es6/Entity.js';
-import { Rels } from '../hypermedia-constants';
+import { Actions, Rels } from '../hypermedia-constants';
+import { performSirenAction } from '../es6/SirenAction';
 
 /**
  * Association class representation of an association.
@@ -14,6 +15,15 @@ export class Association extends Entity {
 
 		return this._entity.getLinkByRel(Rels.Rubrics.rubric).href;
 
+	}
+
+	async deleteAssociation(){
+		if (!this._entity || !this._entity.hasActionByName(Actions.associations.deleteAssociation)) {
+			return;
+		}
+
+		const action = this._entity.getActionByName(Actions.associations.deleteAssociation);
+		await performSirenAction(this._token, action);
 	}
 
 }
