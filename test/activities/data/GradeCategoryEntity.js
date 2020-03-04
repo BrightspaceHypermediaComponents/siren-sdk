fe09107b01cdbd9831147b65474841b332a1aeb7
Vincent Wong <vincent.wong@d2l.com>
2020-03-04 10:15:31 -0800

    Add Grade Category Entity

---
+++ test/activities/data/GradeCategoryEntity.js
@@ -0,0 +1,30 @@
+export const testData = {
+	gradeCategoryEntity: {
+		normal: {
+			'class': [
+				'grade-category'
+			],
+			'rel': [
+				'item',
+				'https://grades.api.brightspace.com/rels/grade-category'
+			],
+			'properties': {
+				'name': 'Category 1'
+			},
+			'links': [
+				{
+					'rel': [
+						'self'
+					],
+					'href': 'https://9caa9c10-0175-4c56-84e5-fc2bca4d8a52.grades.api.proddev.d2l/organizations/6609/grade-categories/5008'
+				},
+				{
+					'rel': [
+						'https://api.brightspace.com/rels/organization'
+					],
+					'href': 'https://9caa9c10-0175-4c56-84e5-fc2bca4d8a52.organizations.api.proddev.d2l/6609'
+				}
+			]
+		}
+	}
+};
