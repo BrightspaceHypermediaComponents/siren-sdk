88a1f3b3b223b08f6d9702e718a3bc2a481c19b8
ten.bradley@d2l.com <Ten.Bradley@D2L.com>
2020-09-22 16:12:58 -0400

    added checking fetch calls to one test

--- test/organizations/CompletionTracking.js
+++ test/organizations/CompletionTracking.js
@@ -48,140 +48,6 @@ describe('Completion tracking', () => {
 		});
 	});
 
-	// describe('update actions, tracking completion, no display', () => {
-	// 	let sandbox, trackingSpy, displaySpy, organization;
-
-	// 	beforeEach(() => {
-	// 		sandbox = sinon.sandbox.create();
-	// 		organization = trackingNoDisplay;
-	// 		trackingSpy = sandbox.spy(organization, 'updateCompletionTracking');
-	// 		displaySpy = sandbox.spy(organization, 'updateDisplayProgress');
-	// 	});
-
-	// 	afterEach(() => {
-	// 		sandbox.restore();
-	// 	});
-
-	// 	it('correct completion parameter', () => {
-	// 		organization.updateCompletionTracking(false);
-	// 		expect(trackingSpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('correct progress parameter', () => {
-	// 		organization.updateDisplayProgress(true);
-	// 		expect(displaySpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('incorrect completion parameter', () => {
-	// 		organization.updateCompletionTracking(true);
-	// 		expect(trackingSpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('incorrect progress parameters', () => {
-	// 		organization.updateDisplayProgress(false);
-	// 		expect(displaySpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// });
-
-	// describe('update actions, tracking completion, tracking display', () => {
-	// 	let sandbox, trackingSpy, displaySpy, organization;
-
-	// 	beforeEach(() => {
-	// 		sandbox = sinon.sandbox.create();
-	// 		organization = trackingAndDisplay;
-	// 		trackingSpy = sandbox.spy(organization, 'updateCompletionTracking');
-	// 		displaySpy = sandbox.spy(organization, 'updateDisplayProgress');
-	// 	});
-
-	// 	afterEach(() => {
-	// 		sandbox.restore();
-	// 	});
-
-	// 	it('correct completion parameter', () => {
-	// 		organization.updateCompletionTracking(false);
-	// 		expect(trackingSpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('correct progress parameter', () => {
-	// 		organization.updateDisplayProgress(false);
-	// 		expect(displaySpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('incorrect completion parameter', () => {
-	// 		organization.updateCompletionTracking(true);
-	// 		expect(trackingSpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('incorrect progress parameter', () => {
-	// 		organization.updateDisplayProgress(true);
-	// 		expect(displaySpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// });
-
-	// describe('update actions, not tracking completion, no display', () => {
-	// 	let sandbox, trackingSpy, displaySpy, organization;
-
-	// 	beforeEach(() => {
-	// 		sandbox = sinon.sandbox.create();
-	// 		organization = noTrackingDisplay;
-	// 		trackingSpy = sandbox.spy(organization, 'updateCompletionTracking');
-	// 		displaySpy = sandbox.spy(organization, 'updateDisplayProgress');
-	// 	});
-
-	// 	afterEach(() => {
-	// 		sandbox.restore();
-	// 	});
-
-	// 	it('correct completion parameter', () => {
-	// 		organization.updateCompletionTracking(true);
-	// 		expect(trackingSpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('correct progress parameter', async() => {
-	// 		await organization.updateDisplayProgress(true);
-	// 		//const resp = displaySpy.returnValues[0];
-	// 		//expect(resp).to.equal(undefined);
-	// 	});
-	// 	it('incorrect completion parameter', () => {
-	// 		organization.updateCompletionTracking(false);
-	// 		expect(trackingSpy.returnValues[0]).to.be.a('promise');
-	// 	});
-	// 	it('incorrect progress parameter', async() => {
-	// 		await organization.updateDisplayProgress(false);
-	// 		const resp = displaySpy.returnValues[0];
-	// 		expect(resp[0]).to.equal(undefined);
-	// 	});
-	// });
-
-	// describe('update actions, actions missing', () => {
-	// 	let sandbox, trackingSpy, displaySpy, organization;
-
-	// 	beforeEach(() => {
-	// 		sandbox = sinon.sandbox.create();
-	// 		organization = noActions;
-	// 		trackingSpy = sandbox.spy(organization, 'updateCompletionTracking');
-	// 		displaySpy = sandbox.spy(organization, 'updateDisplayProgress');
-	// 	});
-
-	// 	afterEach(() => {
-	// 		sandbox.restore();
-	// 	});
-
-	// 	it('true completion parameter', async() => {
-	// 		await organization.updateCompletionTracking(true);
-	// 		const resp = trackingSpy.returnValues[0];
-	// 		expect(resp[0]).to.equal(undefined);
-	// 	});
-	// 	it('true progress parameter', async() => {
-	// 		await organization.updateDisplayProgress(true);
-	// 		const resp = displaySpy.returnValues[0];
-	// 		expect(resp[0]).to.equal(undefined);
-	// 	});
-	// 	it('false parameters', () => {
-	// 		organization.updateCompletionTracking(false);
-	// 		const resp = trackingSpy.returnValues[0];
-	// 		expect(resp[0]).to.equal(undefined);
-	// 	});
-	// 	it('false parameters', () => {
-	// 		organization.updateDisplayProgress(false);
-	// 		const resp = displaySpy.returnValues[0];
-	// 		expect(resp[0]).to.equal(undefined);
-	// 	});
-	// });
-
 	function checkFetchCalls(doNotTrack, track, doNotDisplay, displayed) {
 		expect(fetchMock.called('http://api.x.io/do/not/track/completion')).to.equal(doNotTrack);
 		expect(fetchMock.called('http://api.x.io/track/completion')).to.equal(track);
@@ -208,8 +74,10 @@ describe('Completion tracking', () => {
 				setupMock();
 
 				const resp = await organization.updateCompletionTracking(false);
-				checkFetchCalls(true, false, false, false);
 				expect(resp).to.be.a('object');
+				checkFetchCalls(true, false, false, false);
+				const calls = fetchMock.calls();
+				expect(calls.length).to.be.equal(1);
 			});
 
 			it('track completion', async() => {
