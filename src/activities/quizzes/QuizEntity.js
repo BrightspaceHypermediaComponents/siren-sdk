42147f16bd96ac27b045aff0b1adfac9b914cf3a
Mikayla Louie <Mikayla.Louie@D2L.com>
2021-01-08 15:18:17 -0800

    US123706 fixed formatting and remove debugger statements

--- src/activities/quizzes/QuizEntity.js
+++ src/activities/quizzes/QuizEntity.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { Entity } from '../../es6/Entity';
 import { Actions, Rels, Classes } from '../../hypermedia-constants';
 import { performSirenAction, performSirenActions } from '../../es6/SirenAction';
@@ -11,21 +10,14 @@ export class QuizEntity extends Entity {
 	 * @returns {string} Name of the quiz
 	 */
 	name() {
-		return (
-			this._entity &&
-			this._entity.properties &&
-			this._entity.properties.name
-		);
+		return this._entity && this._entity.properties && this._entity.properties.name;
 	}
 
 	/**
 	 * @returns {bool} Whether or not the edit name action is present on the quiz entity
 	 */
 	canEditName() {
-		return (
-			this._entity &&
-			this._entity.hasActionByName(Actions.quizzes.updateName)
-		);
+		return this._entity && this._entity.hasActionByName(Actions.quizzes.updateName);
 	}
 
 	/**
@@ -33,9 +25,7 @@ export class QuizEntity extends Entity {
 	 * @param {string} name Name to set on the quiz
 	 */
 	async setName(name) {
-		const action =
-			this.canEditName() &&
-			this._entity.getActionByName(Actions.quizzes.updateName);
+		const action = this.canEditName() && this._entity.getActionByName(Actions.quizzes.updateName);
 		if (!action) {
 			return;
 		}
@@ -46,7 +36,7 @@ export class QuizEntity extends Entity {
 
 	/**
 	 * @returns {string} quiz preview href
-	 */
+	*/
 
 	previewHref() {
 		if (!this._entity) {
@@ -60,7 +50,7 @@ export class QuizEntity extends Entity {
 
 	/**
 	 * @returns {bool} Whether or not the user can preview a quiz
-	 */
+	*/
 
 	canPreviewQuiz() {
 		if (!this._entity) {
@@ -88,8 +78,6 @@ export class QuizEntity extends Entity {
 	 */
 	canEditShuffle() {
 		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.shuffle);
-		// eslint-disable-next-line no-debugger
-		debugger;
 		return entity && entity.hasActionByName(Actions.quizzes.updateShuffle);
 	}
 
@@ -105,13 +93,8 @@ export class QuizEntity extends Entity {
 	 * @returns {bool} Whether or not the user can set a password for a quiz entity
 	 */
 	canEditPassword() {
-		const passwordEntity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.password
-		);
-		return (
-			passwordEntity &&
-			passwordEntity.hasActionByName(Actions.quizzes.updatePassword)
-		);
+		const passwordEntity = this._entity.getSubEntityByRel(Rels.Quizzes.password);
+		return passwordEntity && passwordEntity.hasActionByName(Actions.quizzes.updatePassword);
 	}
 
 	/**
@@ -119,10 +102,7 @@ export class QuizEntity extends Entity {
 	 */
 	canEditHints() {
 		const hintsEntity = this._entity.getSubEntityByRel(Rels.Quizzes.hints);
-		return (
-			hintsEntity &&
-			hintsEntity.hasActionByName(Actions.quizzes.updateHints)
-		);
+		return hintsEntity && hintsEntity.hasActionByName(Actions.quizzes.updateHints);
 	}
 
 	/**
@@ -137,22 +117,15 @@ export class QuizEntity extends Entity {
 	 * @returns {bool} Whether or not the user can edit the Disable Right Click quiz entity property
 	 */
 	canEditDisableRightClick() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.disableRightClick
-		);
-		return (
-			entity &&
-			entity.hasActionByName(Actions.quizzes.updateDisableRightClick)
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.disableRightClick);
+		return entity && entity.hasActionByName(Actions.quizzes.updateDisableRightClick);
 	}
 
 	/**
 	 * @returns {bool} Is Disable Right Click checked for the quiz entity
 	 */
 	isDisableRightClickEnabled() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.disableRightClick
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.disableRightClick);
 		return entity && entity.hasClass(Classes.quizzes.checked);
 	}
 
@@ -160,22 +133,15 @@ export class QuizEntity extends Entity {
 	 * @returns {bool} Whether or not the user can edit the Disable Pager and Alerts quiz entity property
 	 */
 	canEditDisablePagerAndAlerts() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.disablePagerAndAlerts
-		);
-		return (
-			entity &&
-			entity.hasActionByName(Actions.quizzes.updateDisablePagerAndAlerts)
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.disablePagerAndAlerts);
+		return entity && entity.hasActionByName(Actions.quizzes.updateDisablePagerAndAlerts);
 	}
 
 	/**
 	 * @returns {bool} Is Disable Pager and Alerts checked for the quiz entity
 	 */
 	isDisablePagerAndAlertsEnabled() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.disablePagerAndAlerts
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.disablePagerAndAlerts);
 		return entity && entity.hasClass(Classes.quizzes.checked);
 	}
 
@@ -183,22 +149,15 @@ export class QuizEntity extends Entity {
 	 * @returns {bool} Whether or not the user can edit the Notification Email property
 	 */
 	canEditNotificationEmail() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.notificationEmail
-		);
-		return (
-			entity &&
-			entity.hasActionByName(Actions.quizzes.updateNotificationEmail)
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.notificationEmail);
+		return entity && entity.hasActionByName(Actions.quizzes.updateNotificationEmail);
 	}
 
 	/**
 	 * @returns {bool} Notification Email of the quiz entity
 	 */
 	notificationEmail() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.notificationEmail
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.notificationEmail);
 		if (!entity || !entity.properties) {
 			return;
 		}
@@ -210,22 +169,15 @@ export class QuizEntity extends Entity {
 	 * @returns {bool} Whether or not the user can edit the preventMovingBackwards property
 	 */
 	canEditPreventMovingBackwards() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.preventMovingBackwards
-		);
-		return (
-			entity &&
-			entity.hasActionByName(Actions.quizzes.updatePreventMovingBackwards)
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.preventMovingBackwards);
+		return entity && entity.hasActionByName(Actions.quizzes.updatePreventMovingBackwards);
 	}
 
 	/**
 	 * @returns {bool} Is prevent moving backwards checked for the quiz entity
 	 */
 	isPreventMovingBackwardsEnabled() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.preventMovingBackwards
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.preventMovingBackwards);
 		return entity && entity.hasClass(Classes.quizzes.checked);
 	}
 
@@ -233,54 +185,29 @@ export class QuizEntity extends Entity {
 	 * @returns {bool} Whether or not the user can edit the autoSetGraded property
 	 */
 	canEditAutoSetGraded() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.autoSetGraded
-		);
-		return (
-			entity &&
-			entity.hasActionByName(Actions.quizzes.updateAutoSetGraded)
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.autoSetGraded);
+		return entity && entity.hasActionByName(Actions.quizzes.updateAutoSetGraded);
 	}
 
 	/**
 	 * @returns {bool} Is AutoSetGraded checked for the quiz entity
 	 */
 	isAutoSetGradedEnabled() {
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.autoSetGraded
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.autoSetGraded);
 		return entity && entity.hasClass(Classes.quizzes.checked);
 	}
 
 	async save(quiz) {
 		if (!quiz) return;
-		const updateNameAction = this.canEditName()
-			? this._formatUpdateNameAction(quiz)
-			: null;
-		const updateShuffleAction = this.canEditShuffle()
-			? this._formatShuffleAction(quiz)
-			: null;
-		const updateHintsAction = this.canEditHints()
-			? this._formatUpdateHintsAction(quiz)
-			: null;
-		const updateDisableRightClickAction = this.canEditDisableRightClick()
-			? this._formatUpdateDisableRightClickAction(quiz)
-			: null;
-		const updateDisablePagerAndAlerts = this.canEditDisablePagerAndAlerts()
-			? this._formatUpdateDisablePagerAndAlerts(quiz)
-			: null;
-		const updatePasswordAction = this.canEditPassword()
-			? this._formatUpdatePasswordAction(quiz)
-			: null;
-		const updateNotificationEmail = this.canEditNotificationEmail()
-			? this._formatNotificationEmailAction(quiz)
-			: null;
-		const updatePreventMovingBackwards = this.canEditPreventMovingBackwards()
-			? this._formatUpdatePreventMovingBackwards(quiz)
-			: null;
-		const updateAutoSetGradedAction = this.canEditAutoSetGraded()
-			? this._formatUpdateAutoSetGraded(quiz)
-			: null;
+		const updateNameAction = this.canEditName() ? this._formatUpdateNameAction(quiz) : null;
+		const updateShuffleAction = this.canEditShuffle() ? this._formatShuffleAction(quiz) : null;
+		const updateHintsAction = this.canEditHints() ? this._formatUpdateHintsAction(quiz) : null;
+		const updateDisableRightClickAction = this.canEditDisableRightClick() ? this._formatUpdateDisableRightClickAction(quiz) : null;
+		const updateDisablePagerAndAlerts = this.canEditDisablePagerAndAlerts() ? this._formatUpdateDisablePagerAndAlerts(quiz) : null;
+		const updatePasswordAction = this.canEditPassword() ? this._formatUpdatePasswordAction(quiz) : null;
+		const updateNotificationEmail = this.canEditNotificationEmail() ? this._formatNotificationEmailAction(quiz) : null;
+		const updatePreventMovingBackwards = this.canEditPreventMovingBackwards() ? this._formatUpdatePreventMovingBackwards(quiz) : null;
+		const updateAutoSetGradedAction = this.canEditAutoSetGraded() ? this._formatUpdateAutoSetGraded(quiz) : null;
 
 		const sirenActions = [
 			updateNameAction,
@@ -291,7 +218,7 @@ export class QuizEntity extends Entity {
 			updatePasswordAction,
 			updateNotificationEmail,
 			updatePreventMovingBackwards,
-			updateAutoSetGradedAction,
+			updateAutoSetGradedAction
 		];
 		await performSirenActions(this._token, sirenActions);
 	}
@@ -327,9 +254,12 @@ export class QuizEntity extends Entity {
 			return;
 		}
 
-		const fields = [{ name: 'allowHints', value: allowHints }];
+		const fields = [
+			{ name: 'allowHints', value: allowHints },
+		];
 
 		return { action, fields };
+
 	}
 
 	/**
@@ -349,7 +279,9 @@ export class QuizEntity extends Entity {
 			return;
 		}
 
-		const fields = [{ name: 'name', value: name }];
+		const fields = [
+			{ name: 'name', value: name },
+		];
 
 		return { action, fields };
 	}
@@ -369,7 +301,9 @@ export class QuizEntity extends Entity {
 		const action = entity.getActionByName(Actions.quizzes.updateShuffle);
 		if (!action) return;
 
-		const fields = [{ name: 'shuffle', value: quiz.shuffle }];
+		const fields = [
+			{ name: 'shuffle', value: quiz.shuffle },
+		];
 
 		return { action, fields };
 	}
@@ -383,14 +317,10 @@ export class QuizEntity extends Entity {
 		if (!quiz) return;
 		if (!this._hasDisableRightClickChanged(quiz.disableRightClick)) return;
 
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.disableRightClick
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.disableRightClick);
 		if (!entity) return;
 
-		const action = entity.getActionByName(
-			Actions.quizzes.updateDisableRightClick
-		);
+		const action = entity.getActionByName(Actions.quizzes.updateDisableRightClick);
 		if (!action) return;
 
 		const fields = [
@@ -407,24 +337,16 @@ export class QuizEntity extends Entity {
 
 	_formatUpdateDisablePagerAndAlerts(quiz) {
 		if (!quiz) return;
-		if (!this._hasDisablePagerAndAlertsChanged(quiz.disablePagerAndAlerts))
-			return;
+		if (!this._hasDisablePagerAndAlertsChanged(quiz.disablePagerAndAlerts)) return;
 
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.disablePagerAndAlerts
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.disablePagerAndAlerts);
 		if (!entity) return;
 
-		const action = entity.getActionByName(
-			Actions.quizzes.updateDisablePagerAndAlerts
-		);
+		const action = entity.getActionByName(Actions.quizzes.updateDisablePagerAndAlerts);
 		if (!action) return;
 
 		const fields = [
-			{
-				name: 'disablePagerAndAlerts',
-				value: quiz.disablePagerAndAlerts,
-			},
+			{ name: 'disablePagerAndAlerts', value: quiz.disablePagerAndAlerts },
 		];
 
 		return { action, fields };
@@ -433,7 +355,7 @@ export class QuizEntity extends Entity {
 	/**
 	 * Checks if quiz password has changed and if so returns the appropriate action/fields to update
 	 * @param {object} quiz the quiz that's being modified
-	 */
+	*/
 
 	_formatUpdatePasswordAction(quiz) {
 		if (!quiz) return;
@@ -445,7 +367,9 @@ export class QuizEntity extends Entity {
 		const action = entity.getActionByName(Actions.quizzes.updatePassword);
 		if (!action) return;
 
-		const fields = [{ name: 'password', value: quiz.password }];
+		const fields = [
+			{ name: 'password', value: quiz.password },
+		];
 
 		return { action, fields };
 	}
@@ -453,19 +377,15 @@ export class QuizEntity extends Entity {
 	/**
 	 * Checks if notification email has changed and if so returns the appropriate action/fields to update
 	 * @param {object} quiz the quiz that's being modified
-	 */
+	*/
 	_formatNotificationEmailAction(quiz) {
 		if (!quiz) return;
 		if (!this._hasNotificationEmailChanged(quiz.notificationEmail)) return;
 
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.notificationEmail
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.notificationEmail);
 		if (!entity) return;
 
-		const action = entity.getActionByName(
-			Actions.quizzes.updateNotificationEmail
-		);
+		const action = entity.getActionByName(Actions.quizzes.updateNotificationEmail);
 		if (!action) return;
 
 		const fields = [
@@ -482,26 +402,16 @@ export class QuizEntity extends Entity {
 
 	_formatUpdatePreventMovingBackwards(quiz) {
 		if (!quiz) return;
-		if (
-			!this._hasPreventMovingBackwardsChanged(quiz.preventMovingBackwards)
-		)
-			return;
+		if (!this._hasPreventMovingBackwardsChanged(quiz.preventMovingBackwards)) return;
 
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.preventMovingBackwards
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.preventMovingBackwards);
 		if (!entity) return;
 
-		const action = entity.getActionByName(
-			Actions.quizzes.updatePreventMovingBackwards
-		);
+		const action = entity.getActionByName(Actions.quizzes.updatePreventMovingBackwards);
 		if (!action) return;
 
 		const fields = [
-			{
-				name: 'preventMovingBackwards',
-				value: quiz.preventMovingBackwards,
-			},
+			{ name: 'preventMovingBackwards', value: quiz.preventMovingBackwards },
 		];
 
 		return { action, fields };
@@ -511,17 +421,15 @@ export class QuizEntity extends Entity {
 		if (!quiz) return;
 		if (!this._hasAutoSetGradedChanged(quiz.autoSetGraded)) return;
 
-		const entity = this._entity.getSubEntityByRel(
-			Rels.Quizzes.autoSetGraded
-		);
+		const entity = this._entity.getSubEntityByRel(Rels.Quizzes.autoSetGraded);
 		if (!entity) return;
 
-		const action = entity.getActionByName(
-			Actions.quizzes.updateAutoSetGraded
-		);
+		const action = entity.getActionByName(Actions.quizzes.updateAutoSetGraded);
 		if (!action) return;
 
-		const fields = [{ name: 'autoSetGraded', value: quiz.autoSetGraded }];
+		const fields = [
+			{ name: 'autoSetGraded', value: quiz.autoSetGraded },
+		];
 
 		return { action, fields };
 	}
@@ -555,9 +463,7 @@ export class QuizEntity extends Entity {
 	}
 
 	_hasPreventMovingBackwardsChanged(preventMovingBackwards) {
-		return (
-			preventMovingBackwards !== this.isPreventMovingBackwardsEnabled()
-		);
+		return preventMovingBackwards !== this.isPreventMovingBackwardsEnabled();
 	}
 
 	_hasAutoSetGradedChanged(autoSetGraded) {
@@ -573,12 +479,9 @@ export class QuizEntity extends Entity {
 			[this.isDisablePagerAndAlertsEnabled(), quiz.disablePagerAndAlerts],
 			[this.password(), quiz.password],
 			[this.notificationEmail(), quiz.notificationEmail],
-			[
-				this.isPreventMovingBackwardsEnabled(),
-				quiz.preventMovingBackwards,
-			],
+			[this.isPreventMovingBackwardsEnabled(), quiz.preventMovingBackwards],
 			[this.isAutoSetGradedEnabled(), quiz.autoSetGraded],
-			[JSON.stringify(this.attempts()), JSON.stringify(quiz.attempts)],
+			[JSON.stringify(this.attempts()), JSON.stringify(quiz.attempts)]
 		];
 
 		for (const [left, right] of diffs) {
@@ -594,9 +497,7 @@ export class QuizEntity extends Entity {
 	}
 
 	async delete() {
-		const action =
-			this.canDelete() &&
-			this._entity.getActionByName(Actions.quizzes.delete);
+		const action = this.canDelete() && this._entity.getActionByName(Actions.quizzes.delete);
 		if (!action) {
 			return;
 		}
