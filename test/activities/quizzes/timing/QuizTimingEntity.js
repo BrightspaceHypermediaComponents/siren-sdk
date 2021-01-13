50a5756c556288cf2e07fe39b9475f6bc06e2dcb
Mikayla Louie <Mikayla.Louie@D2L.com>
2021-01-13 15:54:33 -0800

    US123360 added basic tests for quiz timing entity

---
+++ test/activities/quizzes/timing/QuizTimingEntity.js
@@ -0,0 +1,60 @@
+import { enforcedQuizTiming } from '../data/timing/EnforcedQuizTimingEntity';
+import { recommendedQuizTiming } from '../data/timing/RecommendedQuizTimingEntity';
+import { QuizTimingEntity } from '../../../../src/activities/quizzes/timing/QuizTimingEntity.js';
+
+describe('QuizTimingEntity', () => {
+	var enforcedTimingEntity, recommendedTimingEntity;
+
+	beforeEach(() => {
+		enforcedTimingEntity = window.D2L.Hypermedia.Siren.Parse(enforcedQuizTiming);
+		recommendedTimingEntity = window.D2L.Hypermedia.Siren.Parse(recommendedQuizTiming);
+	});
+
+	describe('can update timing', () => {
+		it('returns true when enforced timing entity can be edited', () => {
+			var entity = new QuizTimingEntity(enforcedTimingEntity);
+			expect(entity.canEditTiming()).to.be.true;
+		});
+		it('returns true when recommended timing entity can be edited', () => {
+			var entity = new QuizTimingEntity(recommendedTimingEntity);
+			expect(entity.canEditTiming()).to.be.true;
+		});
+	});
+
+	describe('timing type', () => {
+		it('returns true when timing is enforced ', () => {
+			var entity = new QuizTimingEntity(enforcedTimingEntity);
+			expect(entity.isTimingEnforced()).to.be.true;
+		});
+		it('returns true when timing is recommended ', () => {
+			var entity = new QuizTimingEntity(recommendedTimingEntity);
+			expect(entity.isTimingEnforced()).to.be.false;
+		});
+	});
+
+	describe('time limits', () => {
+		it('can read enforced time limit', () => {
+			var entity = new QuizTimingEntity(enforcedTimingEntity);
+			expect(entity.enforcedTimeLimit().value).to.equal(120);
+		});
+		it('can read enforced grace period', () => {
+			var entity = new QuizTimingEntity(enforcedTimingEntity);
+			expect(entity.enforcedGraceLimit().value).to.equal(5);
+		});
+		it('can read recommended time limit', () => {
+			var entity = new QuizTimingEntity(recommendedTimingEntity);
+			expect(entity.recommendedTimeLimit()).to.equal(120);
+		});
+	});
+
+	describe('show clock', () => {
+		it('return true when showClock is checked', () => {
+			var entity = new QuizTimingEntity(recommendedTimingEntity);
+			expect(entity.showClock()).to.be.true;
+		});
+		it('can read Show clock label', () => {
+			var entity = new QuizTimingEntity(recommendedTimingEntity);
+			expect(entity.showClockTitle()).to.equal('Show clock');
+		});
+	});
+});
