e540754262341fe4d5e1011cd53148aac2db8b04
ChrisKraljevicD2L <chris.kraljevic@d2l.com>
2021-02-04 14:13:11 -0600

    Add back in LTI link entity tests

---
+++ test/activities/content/ContentLTILinkEntity.js
@@ -0,0 +1,39 @@
+/* global fetchMock */
+import { ContentLTILinkEntity } from '../../../src/activities/content/contentLTILinkEntity.js';
+import { contentLTILinkData } from './data/TestContentLTILinkEntity.js';
+
+describe('ContentLTILinkEntity', () => {
+	let ltiLinkData;
+	let contentLTILinkEntity;
+
+	beforeEach(() => {
+		ltiLinkData = window.D2L.Hypermedia.Siren.Parse(contentLTILinkData);
+		contentLTILinkEntity = new ContentLTILinkEntity(ltiLinkData);
+	});
+
+	afterEach(() => {
+		fetchMock.reset();
+	});
+
+	describe('Reads properties', () => {
+		it('reads title', () => {
+			expect(contentLTILinkEntity.title()).to.equal('Test LTI Link Title');
+		});
+
+		it('reads rich text description', () => {
+			expect(contentLTILinkEntity.descriptionRichText()).to.equal('<p>description text</p>');
+		});
+
+		it('reads text description', () => {
+			expect(contentLTILinkEntity.descriptionText()).to.equal('description text');
+		});
+
+		it('reads url', () => {
+			expect(contentLTILinkEntity.url()).to.equal('https://phoenix-is-the-best.com');
+		});
+
+		it('reads isExternalResources', () => {
+			expect(contentLTILinkEntity.isExternalResource()).to.equal(true);
+		});
+	});
+});
---
+++ test/activities/content/data/TestContentLTILinkEntity.js
@@ -0,0 +1,62 @@
+export const contentLTILinkData = {
+	'actions': [
+		{
+			'href': 'https://fake-tenant-id.ltilinks.api.proddev.d2l/6613/ltilinks/12345',
+			'name': 'update-title',
+			'method': 'PATCH',
+			'fields': [
+				{
+					'class': ['required'],
+					'type': 'text',
+					'name': 'title',
+					'value': 'Test Web Link Title'
+				}
+			]
+		},
+		{
+			'href': 'https://fake-tenant-id.ltilinks.api.proddev.d2l/6613/ltilinks/12345',
+			'name': 'update-description',
+			'method': 'PATCH',
+			'fields': [
+				{
+					'class': ['required'],
+					'type': 'text',
+					'name': 'description',
+					'value': '<p>description text</p>'
+				}
+			]
+		},
+		{
+			'href': 'https://fake-tenant-id.ltilinks.api.proddev.d2l/6613/ltilinks/12345/external',
+			'name': 'update-external-resource',
+			'method': 'PUT',
+			'fields': [
+				{
+					'type': 'checkbox',
+					'name': 'isExternalResource',
+					'value': true
+				}
+			]
+		},
+		{
+			'href': 'https://fake-tenant-id.ltilinks.api.proddev.d2l/6613/ltilinks/12345',
+			'name': 'delete-ltiLink',
+			'method': 'DELETE'
+		}
+	],
+	'class': [
+		'describable-entity',
+		'topic',
+		'external-resource'
+	],
+	'properties': {
+		'title': 'Test LTI Link Title',
+		'url': 'https://phoenix-is-the-best.com'
+	},
+	'entities': [{
+		'class': ['richtext', 'description'],
+		'properties': {'text': 'description text', 'html': '<p>description text</p>'},
+		'rel': []
+	}],
+	'rel': []
+};
