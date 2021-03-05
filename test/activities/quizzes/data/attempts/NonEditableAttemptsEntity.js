a0cdc32aad17383e0f443aca560bed5051eef178
Andrew Wong <Andrew.Wong@D2L.com>
2021-03-04 18:06:03 -0800

    Add non editable entity

---
+++ test/activities/quizzes/data/attempts/NonEditableAttemptsEntity.js
@@ -0,0 +1,71 @@
+export const nonEditableQuizAttemptsEntity = {
+	'class': [
+		'attempts'
+	],
+	'properties': {
+		'attempts': '3'
+	},
+	'entities': [
+		{
+			'class': [
+				'overall-grade-calculation-type'
+			],
+			'rel': [
+				'related'
+			],
+			'properties': {
+				'overallGradeCalculationType': {
+					'title': 'Lowest Attempt',
+					'value': 'lowest'
+				}
+			}
+		},
+		{
+			'class': [
+				'retake-incorrect-only',
+				'checked'
+			],
+			'rel': [
+				'related'
+			]
+		},
+		{
+			'class': [
+				'attempt-conditions',
+				'collection'
+			],
+			'rel': [
+				'related'
+			],
+			'entities': [
+				{
+					'class': [
+						'attempt-condition'
+					],
+					'rel': [
+						'item'
+					]
+				},
+				{
+					'class': [
+						'attempt-condition'
+					],
+					'rel': [
+						'item'
+					]
+				}
+			]
+		}
+	],
+	'links': [
+		{
+			'rel': [
+				'self'
+			],
+			'href': 'https://tenantId.quizzes.api.dev.brightspace.com/123170/quizzes/1391/attempts'
+		}
+	],
+	'rel': [
+		'https://quizzes.api.brightspace.com/rels/attempts'
+	]
+};
