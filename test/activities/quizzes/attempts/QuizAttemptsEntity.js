0632a603a982067ed1c0bebe8548ae096445ad93
Andrew Wong <Andrew.Wong@D2L.com>
2021-03-05 02:17:19 -0800

    Add non editable entity tests

--- test/activities/quizzes/attempts/QuizAttemptsEntity.js
+++ test/activities/quizzes/attempts/QuizAttemptsEntity.js
@@ -1,31 +1,37 @@
 import { quizAttemptsEntity } from '../data/attempts/EditableEntity.js';
-import { nonEditableEntity } from '../data/attempts/NonEditableEntity.js';
+import { nonEditableAttemptsEntity } from '../data/attempts/NonEditableEntity.js';
 import { QuizAttemptsEntity } from '../../../../src/activities/quizzes/attempts/QuizAttemptsEntity.js';
 
 describe('attempts', () => {
-	let entityJson;
+	let editableEntity, nonEditableEntity;
 
 	beforeEach(() => {
-		entityJson = window.D2L.Hypermedia.Siren.Parse(quizAttemptsEntity);
+		editableEntity = window.D2L.Hypermedia.Siren.Parse(quizAttemptsEntity);
+		nonEditableEntity = window.D2L.Hypermedia.Siren.Parse(nonEditableAttemptsEntity);
 	});
 
 	describe('canUpdateAttemptsAllowed', () => {
 		it('returns true when attempts allowed is editable', () => {
-			const entity = new QuizAttemptsEntity(entityJson);
+			const entity = new QuizAttemptsEntity(editableEntity);
 			expect(entity.canUpdateAttemptsAllowed()).to.be.true;
+		});
+
+		it('returns false when attempts allowed is uneditable', () => {
+			const entity = new QuizAttemptsEntity(nonEditableEntity);
+			expect(entity.canUpdateAttemptsAllowed()).to.be.false;
 		});
 	});
 
 	describe('attempts', () => {
 		it('can read number of attempts allowed', () => {
-			const entity = new QuizAttemptsEntity(entityJson);
+			const entity = new QuizAttemptsEntity(editableEntity);
 			expect(entity.attemptsAllowed()).to.equal('3');
 		});
 	});
 
 	describe('attempts', () => {
 		it('can read number of attempts allowed', () => {
-			const entity = new QuizAttemptsEntity(entityJson);
+			const entity = new QuizAttemptsEntity(editableEntity);
 			const attemptOptions = entity.attemptsAllowedOptions();
 			expect(attemptOptions).to.be.an('array');
 			expect(attemptOptions.length).to.equal(11);
@@ -34,22 +40,28 @@ describe('attempts', () => {
 });
 
 describe('overall grade calculation', () => {
-	let entityJson;
+	let editableEntity, nonEditableEntity;
 
 	beforeEach(() => {
-		entityJson = window.D2L.Hypermedia.Siren.Parse(quizAttemptsEntity);
+		editableEntity = window.D2L.Hypermedia.Siren.Parse(quizAttemptsEntity);
+		nonEditableEntity = window.D2L.Hypermedia.Siren.Parse(nonEditableAttemptsEntity);
 	});
 
 	describe('canUpdateOverallGradeCalculation', () => {
 		it('returns true when overall grade calculation is editable', () => {
-			const entity = new QuizAttemptsEntity(entityJson);
+			const entity = new QuizAttemptsEntity(editableEntity);
 			expect(entity.canUpdateOverallGradeCalculation()).to.be.true;
+		});
+
+		it('returns false when overall grade calculation is uneditable', () => {
+			const entity = new QuizAttemptsEntity(nonEditableEntity);
+			expect(entity.canUpdateOverallGradeCalculation()).to.be.false;
 		});
 	});
 
 	describe('attempts', () => {
 		it('can read overall grade calculation options', () => {
-			const entity = new QuizAttemptsEntity(entityJson);
+			const entity = new QuizAttemptsEntity(editableEntity);
 			expect(entity.overallGradeCalculationOptions().length).to.equal(5);
 		});
 	});
