c4c9a5635c60c2547798e495cbfa0112dfebb321
Ronan Fegan <rfegan13@gmail.com>
2021-04-09 14:25:15 -0700

    Added methods for categories

--- src/activities/CategoriesEntity.js
+++ src/activities/CategoriesEntity.js
@@ -1,5 +1,5 @@
-import { Entity } from '../es6/Entity';
-import { Classes } from '../hypermedia-constants.js';
+import { Entity } from '../es6/Entity.js';
+import { Classes, Actions } from '../hypermedia-constants.js';
 // import { performSirenAction } from '../es6/SirenAction';
 
 /**
@@ -17,8 +17,37 @@ export class CategoriesEntity extends Entity {
 
 		return this._entity.getSubEntitiesByClass(Classes.assignments.category).map((category, index) => ({
 			name: category.properties.name,
-			selected: category.hasClass('selected'),
+			selected: category.hasClass(Classes.assignments.selected),
 			index
 		}));
+	}
+
+	/**
+	 * @returns {Boolean} Whether or not a user can edit categories
+	*/
+	canEditCategories() {
+		if (!this._entity) {
+			return;
+		}
+
+		return !!this._entity.getActionByName(Actions.assignments.add);
+	}
+
+	/**
+	 * @returns {Object} The currently selected category
+	*/
+	getSelectedCategory() {
+		if (!this._entity) {
+			return;
+		}
+
+		const subEntities = this._entity.getSubEntitiesByClass(Classes.assignments.category);
+
+		let selectedCategory;
+		if (subEntities) {
+			selectedCategory = subEntities.find(category => category.class.includes(Classes.assignments.selected));
+		}
+
+		return selectedCategory;
 	}
 }
