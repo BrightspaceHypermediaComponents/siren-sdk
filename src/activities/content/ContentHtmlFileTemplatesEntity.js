96314f8c3e95db9bd944a61c938a32d02dd43575
Quinn Budan <Quinn.Budan@D2L.com>
2021-06-30 14:04:27 -0400

    US127570 - move title function to file entity

--- src/activities/content/ContentHtmlFileTemplatesEntity.js
+++ src/activities/content/ContentHtmlFileTemplatesEntity.js
@@ -4,14 +4,6 @@ import { Entity } from '../../es6/Entity';
  * ContentHtmlFileTemplatesEntity class representation of an html file templates entity.
  */
 export class ContentHtmlFileTemplatesEntity extends Entity {
-
-	/**
-	 * @returns {string|undefined} Title of the content HTML file template entity
-	 */
-	title() {
-		return this._entity && this._entity.properties && this._entity.properties.title;
-	}
-
 	/**
 	 * @returns {array|undefined} Array containing the subentites of the file template entity, if they exist
 	 */
