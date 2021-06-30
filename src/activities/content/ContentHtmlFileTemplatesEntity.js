d32fd96418827841fba92db426d62935c30edb3e
Quinn Budan <Quinn.Budan@D2L.com>
2021-06-30 12:57:54 -0400

    US127570 - update template entity definition to provide a title function

--- src/activities/content/ContentHtmlFileTemplatesEntity.js
+++ src/activities/content/ContentHtmlFileTemplatesEntity.js
@@ -6,6 +6,13 @@ import { Entity } from '../../es6/Entity';
 export class ContentHtmlFileTemplatesEntity extends Entity {
 
 	/**
+	 * @returns {string|undefined} Title of the content HTML file template entity
+	 */
+	title() {
+		return this._entity && this._entity.properties && this._entity.properties.title;
+	}
+
+	/**
 	 * @returns {array|undefined} Array containing the subentites of the file template entity, if they exist
 	 */
 	getHtmlFileTemplates() {
