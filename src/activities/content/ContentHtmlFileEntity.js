a18ff57de26d445f8a439ac15e889cf34faffa4c
brissons <52549862+brissons@users.noreply.github.com>
2021-07-21 10:07:36 -0500

    fix: add font size property and move templates to html entity (#361)

--- src/activities/content/ContentHtmlFileEntity.js
+++ src/activities/content/ContentHtmlFileEntity.js
@@ -1,11 +1,26 @@
-import { Actions } from '../../hypermedia-constants.js';
+import { Actions, Rels } from '../../hypermedia-constants.js';
 import { performSirenAction } from '../../es6/SirenAction.js';
 import { ContentFileEntity } from './ContentFileEntity.js';
+import ContentHelperFunctions from './ContentHelperFunctions.js';
 
 /**
  *  ContentHtmlFileEntity class representation of a d2l html content-file entity.
  */
 export class ContentHtmlFileEntity extends ContentFileEntity {
+	/**
+	 * @returns {string|undefined} Default font-size for the html editor
+	 */
+	fontSize() {
+		return this._entity && this._entity.properties && this._entity.properties.contentFontSize;
+	}
+
+	/**
+	 * @returns {string|null} html templates href
+	 */
+	getHtmlTemplatesHref() {
+		return ContentHelperFunctions.getHrefFromRel(Rels.Content.htmlTemplates, this._entity);
+	}
+
 	/**
 	 * Updates the html file content with the given html
 	 * @param {html} html to set on the html file
