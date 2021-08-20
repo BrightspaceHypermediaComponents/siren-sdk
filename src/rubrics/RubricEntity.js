963cd32aa64ca8771aa8e06039f3c2508d01120a
Emil Furniss <32819802+emil-furniss@users.noreply.github.com>
2021-08-20 17:42:02 +0100

    feat: create RubricEntity class (#370)

---
+++ src/rubrics/RubricEntity.js
@@ -0,0 +1,14 @@
+import { Classes } from '../hypermedia-constants';
+import { Entity } from '../es6/Entity.js';
+
+export class RubricEntity extends Entity {
+	name() {
+		return this._entity && this._entity.properties && this._entity.properties.name;
+	}
+	rubricId() {
+		return this._entity && this._entity.properties && this._entity.properties.rubricId;
+	}
+	isTextOnly() {
+		return this._entity && this._entity.hasClass(Classes.rubrics.text);
+	}
+}
