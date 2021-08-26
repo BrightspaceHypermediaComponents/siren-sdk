695b426831529a822b51e8db6431b49b493e371f
Maddie Gabriel <Maddie.Gabriel@D2L.com>
2021-08-26 13:41:12 -0400

    linter

--- src/files/RestrictedEntity.js
+++ src/files/RestrictedEntity.js
@@ -5,10 +5,10 @@ import { Entity } from '../es6/Entity';
  */
 export class RestrictedEntity extends Entity {
 
-    /**
-     * @returns {object} Returns the list of restricted extensions
-     */
-    extensions() {
-        return this._entity && this._entity.properties && this._entity.properties.extensions;
-    }
+	/**
+	 * @returns {object} Returns the list of restricted extensions
+	 */
+	extensions() {
+		return this._entity && this._entity.properties && this._entity.properties.extensions;
+	}
 }
