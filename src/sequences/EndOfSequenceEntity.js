974b2827cca7ff4375bd78698f89b3123aae4b35
ianlaidlaw <laidlaw434@gmail.com>
2022-01-28 10:58:45 -0600

    pr comments, add return comment

--- src/sequences/EndOfSequenceEntity.js
+++ src/sequences/EndOfSequenceEntity.js
@@ -1,6 +1,9 @@
 import { Entity } from '../es6/Entity';
 
 export class EndOfSequenceActivityEntity extends Entity {
+	/**
+	 * @return An object with numeric values: optional, required
+	 */
 	endOfSequenceSummaryProperties() {
 		if (!this._entity) {
 			return {};
