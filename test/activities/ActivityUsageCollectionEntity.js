7297bb33fbd7b98d93bdff3baf90bbad757d2a08
Candice Pang <Candice.Pang@D2L.com>
2022-04-13 16:05:22 -0700

    US135988 Added tests for set-collection-paging action

--- test/activities/ActivityUsageCollectionEntity.js
+++ test/activities/ActivityUsageCollectionEntity.js
@@ -1,10 +1,12 @@
+/* global fetchMock */
 import { ActivityUsageCollectionEntity } from '../../src/activities/ActivityUsageCollectionEntity.js';
-import { collection } from './data/collection.js';
+import { getFormData } from '../utility/test-helpers.js';
+import { testData } from './data/collection.js';
 
 describe('ActivityUsageCollectionEntity', () => {
 	describe('Basic loading', () => {
 		it('Loads collected items', done => {
-			const entity = window.D2L.Hypermedia.Siren.Parse(collection);
+			const entity = window.D2L.Hypermedia.Siren.Parse(testData.collection);
 			const collectionEntity = new ActivityUsageCollectionEntity(entity);
 
 			const itemEntities = [];
@@ -18,7 +20,7 @@ describe('ActivityUsageCollectionEntity', () => {
 		});
 
 		it('has activity-usage href for collected items', done => {
-			const entity = window.D2L.Hypermedia.Siren.Parse(collection);
+			const entity = window.D2L.Hypermedia.Siren.Parse(testData.collection);
 			const collectionEntity = new ActivityUsageCollectionEntity(entity);
 
 			const itemEntities = [];
@@ -31,6 +33,74 @@ describe('ActivityUsageCollectionEntity', () => {
 					'https://activities.api.testdomain.d2l/activities/6606_3000_6/usages/6606'
 				);
 				done();
+			});
+		});
+	});
+
+	describe('Set Collection Paging', () => {
+		describe('Editable', () => {
+			let entity;
+			let entityJson;
+
+			beforeEach(() => {
+				entityJson = window.D2L.Hypermedia.Siren.Parse(testData.setCollectionPagingEditable);
+				entity = new ActivityUsageCollectionEntity(entityJson);
+			});
+
+			afterEach(() => {
+				fetchMock.reset();
+			});
+
+			it('sets canUpdatePagingType to true', () => {
+				expect(entity.canUpdatePagingType()).to.be.true;
+			});
+
+			it('saves pagingType', async() => {
+				fetchMock.patchOnce('https://305d9474-d93a-4205-9d27-101205376fcf.activities.api.dev.brightspace.com/old/activities/6606_51000_1759/usages/123171/collection/paging', entityJson);
+
+				await entity.save({
+					selectedPagingType: 'none'
+				});
+
+				const form = await getFormData(fetchMock.lastCall().request);
+				if (!form.notSupported) {
+					expect(form.get('pagingType')).to.equal('none');
+				}
+				expect(fetchMock.called()).to.be.true;
+			});
+
+			it('skips save if not dirty', async() => {
+				await entity.save({
+					selectedPagingType: 'oneactivityperpage'
+				});
+
+				expect(fetchMock.done());
+			});
+		});
+
+		describe('Non Editable', () => {
+			let entity;
+			let entityJson;
+
+			beforeEach(() => {
+				entityJson = window.D2L.Hypermedia.Siren.Parse(testData.setCollectionPagingNonEditable);
+				entity = new ActivityUsageCollectionEntity(entityJson);
+			});
+
+			afterEach(() => {
+				fetchMock.reset();
+			});
+
+			it('sets canUpdatePagingType to false', () => {
+				expect(entity.canUpdatePagingType()).to.be.false;
+			});
+
+			it('skips save if not editable', async() => {
+				await entity.save({
+					selectedPagingType: 'none'
+				});
+
+				expect(fetchMock.done());
 			});
 		});
 	});
