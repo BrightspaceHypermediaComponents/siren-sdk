03a3d1fff5c49f964fd4bb9e7a684dc0f501a3d6
ianlaidlaw <laidlaw434@gmail.com>
2022-05-31 13:22:01 -0500

    create the multiple associated grades entity

---
+++ src/activities/associateGrade/AssociateMultipleGradesEntity.js
@@ -0,0 +1,23 @@
+// import { Actions, Classes, Rels } from '../../hypermedia-constants.js';
+import { AssociateGradeEntity } from './AssociateGradeEntity.js';
+import { Entity } from '../../es6/Entity.js';
+// import { GradeCandidateCollectionEntity } from '../GradeCandidateCollectionEntity.js';
+// import { GradeCategoryCollectionEntity } from './GradeCategoryCollectionEntity.js';
+// import { GradeSchemeCollectionEntity } from './GradeSchemeCollectionEntity.js';
+// import { performSirenAction } from '../../es6/SirenAction.js';
+
+/**
+ * AssociateGrade entity of an activity.
+ */
+export class AssociateMultipleGradesEntity extends Entity {
+	getAssociateGrades() {
+		return this._entity.getSubEntitiesByRel('what is the rel?').map(subEntity => {
+			const associateGradeEntity = new AssociateGradeEntity(subEntity);
+
+			// eslint-disable-next-line no-console
+			console.log({ AssociateGradeEntity });
+
+			return associateGradeEntity;
+		});
+	}
+}
