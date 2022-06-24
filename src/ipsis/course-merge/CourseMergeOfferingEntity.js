53e2b15cb30de0b1fa63a8e1b847c5f959e48c41
Duo Wang <duo.wang@d2l.com>
2022-06-24 14:34:43 -0400

    add org href

--- src/ipsis/course-merge/CourseMergeOfferingEntity.js
+++ src/ipsis/course-merge/CourseMergeOfferingEntity.js
@@ -3,15 +3,21 @@
  * See: ISirenCourseMergeSerializer.SerializeCourseOfferingResult
  */
 import { Entity } from '../../es6/Entity.js';
+import { Rels } from '../hypermedia-constants.js';
 
 export class CourseMergeOfferingEntity extends Entity {
 	ownedByMultipleSourceSystems() {
 		return this._entity?.properties?.ownedByMultipleSourceSystems;
 	}
+
 	orgUnitId() {
 		return this._entity?.properties?.orgUnitId;
 	}
+
 	_organizationHref() {
-		// TODO: Figure this out
+		if (!this._entity || !this._entity.hasLinkByRel(Rels.organization)) {
+			return;
+		}
+		return this._entity.getLinkByRel(Rels.organization).href;
 	}
 }
