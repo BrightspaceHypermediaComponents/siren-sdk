346c89a0aa101168c0f9b9a627928b79e709b941
Candice Pang <Candice.Pang@D2L.com>
2022-07-08 12:28:00 -0700

    US140433 Added action formatter for updating name and generic function for checking if value has changed

--- src/activities/discussions/DiscussionTopicEntity.js
+++ src/activities/discussions/DiscussionTopicEntity.js
@@ -19,4 +19,26 @@ export class DiscussionTopicEntity extends Entity {
 	canEditName() {
 		return this._entity && this._entity.hasActionByName(Actions.discussions.topic.updateName);
 	}
+
+	/**
+	 * @summary Formats action and fields if topic name has changed and user has edit permission
+	 * @param {object} topic the topic that's being modified
+	 * @returns {object} the appropriate action/fields to update
+	 */
+	_formatUpdateNameAction(topic) {
+		const { name } = topic || {};
+
+		if (!name) return;
+		if (!this._hasFieldValueChanged(name, this.name())) return;
+		if (!this.canEditName()) return;
+
+		const action = this._entity.getActionByName(Actions.discussions.topic.updateName);
+		const fields = [{ name: 'name', value: name }];
+
+		return { action, fields };
+	}
+
+	_hasFieldValueChanged(currentValue, initialValue) {
+		return currentValue !== initialValue;
+	}
 }
