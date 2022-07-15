eb70e0a63b8a20771382d84f95476a03e2387b68
ianlaidlaw <laidlaw434@gmail.com>
2022-07-15 09:47:20 -0500

    cleanup

--- src/activities/associateGrade/AssociateMultipleGradesEntity.js
+++ src/activities/associateGrade/AssociateMultipleGradesEntity.js
@@ -1,35 +1,34 @@
-import { Entity } from '../../es6/Entity.js';
 import { Actions } from '../../hypermedia-constants.js';
+import { Entity } from '../../es6/Entity.js';
 import { performSirenAction } from '../../es6/SirenAction.js';
-// import { GradeCandidateCollectionEntity } from '../GradeCandidateCollectionEntity.js';
-// import { GradeCategoryCollectionEntity } from './GradeCategoryCollectionEntity.js';
-// import { GradeSchemeCollectionEntity } from './GradeSchemeCollectionEntity.js';
-// import { performSirenAction } from '../../es6/SirenAction.js';
 
 /**
  * AssociateGrade entity of an activity.
  */
 export class AssociateMultipleGradesEntity extends Entity {
+	/**
+	 * @returns {array} siren representations of all AssociateGradeEntities
+	 */
 	getAssociateGrades() {
-		return this._entity.getSubEntitiesByRel('existing-grade').map(subEntity => {
-			return subEntity;
-			// const associateGradeEntity = new AssociateGradeEntity(subEntity);
-			//
-			// // eslint-disable-next-line no-console
-			// console.log({ AssociateGradeEntity });
-			//
-			// return associateGradeEntity;
-		});
+		return this._entity.getSubEntitiesByRel('existing-grade');
 	}
 
-	async createNewGrade() {
-		// if (!this.canEditGradebookStatus()) return;
-
+	/**
+	 * Creates a new grade object to be associated with this topic
+	 */
+	async createNewGrade(gradeName) {
 		const action = this._entity.getActionByName(Actions.activities.associateMultipleGrades.gradebookStatus);
-		const fields = [{ name: 'gradebookStatus', value: 'new-grade' }];
+		const fields = [
+			{ name: 'gradebookStatus', value: 'new-grade' },
+			{ name: 'gradeName', value: gradeName }
+		];
+
 		const returnedEntity = await performSirenAction(this._token, action, fields);
 
-		if (!returnedEntity) return;
+		if (!returnedEntity) {
+			return;
+		}
+
 		return new AssociateMultipleGradesEntity(returnedEntity, this._token);
 	}
 }
