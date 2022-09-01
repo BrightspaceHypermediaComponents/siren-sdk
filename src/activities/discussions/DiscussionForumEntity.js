140230dddc1c78e2e3b55a368544adcb2ff1f5f0
Andrew Wong <Andrew.Wong@D2L.com>
2022-09-01 15:54:17 -0700

    US142491 Add Discussion Forum entity

---
+++ src/activities/discussions/DiscussionForumEntity.js
@@ -0,0 +1,73 @@
+import { Actions } from '../../hypermedia-constants.js';
+import { Entity } from '../../es6/Entity.js';
+import { performSirenActions } from '../../es6/SirenAction.js';
+
+/**
+ * DiscussionForumEntity class representation of a D2L Discussion Forum.
+ */
+export class DiscussionForumEntity extends Entity {
+	/**
+	 * @returns {string} Name of the discussion forum
+	 */
+	name() {
+		return this._entity && this._entity.properties && this._entity.properties.name;
+	}
+
+	/**
+	 * @summary Formats action and fields if forum name has changed and user has edit permission
+	 * @param {object} forum the forum that's being modified
+	 * @returns {object} the appropriate action/fields to update
+	 */
+	_formatUpdateNameAction(forum) {
+		const { name } = forum || {};
+
+		if (!name) return;
+		if (!this._hasFieldValueChanged(name, this.name())) return;
+		if (!this.canEditName()) return;
+
+		const action = this._entity.getActionByName(Actions.discussions.forum.updateName);
+		const fields = [
+			{ name: 'name', value: name },
+		];
+
+		return { action, fields };
+	}
+
+	/**
+	 * @summary Checks if forum entity has changed, primarily used for dirty check
+	 * @param {object} forum the forum that's being modified
+	 */
+	equals(forum) {
+		const diffs = [
+			[forum.name, this.name()],
+		];
+
+		for (const [current, initial] of diffs) {
+			if (current !== initial) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @summary Fires all the formatted siren actions collectively
+	 * @param {object} forum the forum that's being modified
+	 */
+	async save(forum) {
+		if (!forum) return;
+
+		const updateNameAction = this._formatUpdateNameAction(forum);
+
+		const sirenActions = [
+			updateNameAction,
+		];
+
+		await performSirenActions(this._token, sirenActions);
+	}
+
+	_hasFieldValueChanged(currentValue, initialValue) {
+		return currentValue !== initialValue;
+	}
+}
