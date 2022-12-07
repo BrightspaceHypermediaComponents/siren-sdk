fd77364b861705693da5a2fa3b127066c610514c
Chiragkumar Prajapati <Chiragkumar.Prajapati@D2L.com>
2022-12-07 12:56:56 -0600

    feat: US145707: [Continued] UCE -> Move the Manual Completion component to Activities (#556)

---
+++ src/activities/content/ContentCompletionEntity.js
@@ -0,0 +1,34 @@
+import { Entity } from '../../es6/Entity.js';
+import { performSirenAction } from '../../es6/SirenAction.js';
+
+/**
+ * ContentCompletionEntity class representation of a d2l content completion entity.
+ */
+export class ContentCompletionEntity extends Entity {
+
+	/** @returns {bool} Whether or not the the content completion type is manual*/
+	isContentCompletionManual() {
+		return this._entity && this._entity.hasClass('manual');
+	}
+
+	/** @returns {bool} Whether or not the the content has been marked as completed*/
+	isContentCompleted() {
+		return this._entity && this._entity.hasClass('completed');
+	}
+
+	/**
+	 * Updates the completion status to completed
+	 */
+	async markAsComplete() {
+		if (!this._entity) {
+			return;
+		}
+		const action = this._entity.getActionByName('mark-as-complete');
+		if (!action) {
+			return;
+		}
+
+		const res = await performSirenAction(this._token, action);
+		return res;
+	}
+}
