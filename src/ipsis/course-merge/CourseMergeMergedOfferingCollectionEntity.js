2ca9280ab82a22b107022bcf41ae034741232195
Zak Wilson <zak.wilson@D2L.com>
2023-01-18 13:38:09 -0500

    Add paging properties

--- src/ipsis/course-merge/CourseMergeMergedOfferingCollectionEntity.js
+++ src/ipsis/course-merge/CourseMergeMergedOfferingCollectionEntity.js
@@ -15,6 +15,61 @@ export class CourseMergeMergedOfferingCollectionEntity extends Entity {
 		return this._entity?.getSubEntitiesByClass(Classes.ipsis.sisCourseMerge.originalTarget)?.[0];
 	}
 
+	prependOriginalSourceCourseMergeOfferings(previousCourseMergeMergedOfferingCollectionEntity) {
+		this._entity.entities.unshift(...previousCourseMergeMergedOfferingCollectionEntity.originalSourceCourseMergeOfferings());
+	}
+
+	totalCount() {
+		return this._pagingInfo()?.totalCount;
+	}
+
+	page() {
+		return this._pagingInfo()?.page;
+	}
+
+	pageSize() {
+		return this._pagingInfo()?.pageSize;
+	}
+
+	loadMorePageSize() {
+		const pageSize = this.pageSize();
+		const totalCount = this.totalCount() ?? 0;
+		const courseMergeOfferingsLength = this._entity?.entities?.length ?? 0;
+		// if pageSize is larger than the number remaining items, return the number of remaining items to be loaded
+		if (totalCount < courseMergeOfferingsLength + (pageSize ?? 0)) {
+			return totalCount - courseMergeOfferingsLength;
+		}
+		return pageSize;
+	}
+
+	_pagingInfo() {
+		return this._entity?.properties?.pagingInfo;
+	}
+
+	hasNextPage() {
+		return this._entity.hasLinkByRel('next');
+	}
+
+	hasPrevPage() {
+		return this._entity.hasLinkByRel('prev');
+	}
+
+	nextPageHref() {
+		if (!this.hasNextPage()) {
+			return;
+		}
+
+		return this._entity.getLinkByRel('next').href;
+	}
+
+	prevPageHref() {
+		if (!this.hasPrevPage()) {
+			return;
+		}
+
+		return this._entity.getLinkByRel('prev').href;
+	}
+
 	userOwnedByMultipleSourceSystems() {
 		return this._entity?.properties?.userOwnedByMultipleSourceSystems;
 	}
