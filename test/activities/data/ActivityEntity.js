7137610c18fb52cf96de4448b4962040e2709dc1
Margaret Dulat <margaret.dulat@d2l.com>
2023-06-20 14:50:50 -0700

    add tests

---
+++ test/activities/data/ActivityEntity.js
@@ -0,0 +1,62 @@
+export const testData = {
+	activityEntityEditable: {
+		'class': ['activity'],
+		'entities': [
+			{
+				'class': [
+					'activity-name'
+				],
+				'properties': {
+					'name': 'Sample Activity Name'
+				},
+				'actions': [
+					{
+						'href': 'https://d53287c1-ad48-4c2e-a0a7-bd53c3029e95.activities.api.dev.brightspace.com/activities/5',
+						'name': 'update-name',
+						'method': 'PATCH',
+						'fields': [
+							{
+								'type': 'text',
+								'name': 'name',
+								'value': 'Sample Activity Name'
+							}
+						]
+					}
+				],
+				'rel': ['activity-name']
+			}
+		],
+
+		'links': [
+			{
+				'rel': [
+					'self'
+				],
+				'href': 'https://d53287c1-ad48-4c2e-a0a7-bd53c3029e95.activities.api.dev.brightspace.com/activities/5'
+			}
+		]
+	},
+	activityEntityReadOnly: {
+		'class': ['activity'],
+		'entities': [
+			{
+				'class': [
+					'activity-name'
+				],
+				'properties': {
+					'name': 'Sample Activity Name'
+				},
+				'rel': ['activity-name']
+			}
+		],
+
+		'links': [
+			{
+				'rel': [
+					'self'
+				],
+				'href': 'https://d53287c1-ad48-4c2e-a0a7-bd53c3029e95.activities.api.dev.brightspace.com/activities/5'
+			}
+		]
+	}
+};
