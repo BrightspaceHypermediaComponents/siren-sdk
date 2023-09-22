1ac786eaa62dd68c04744c887ab9ebf33e4c584d
Kajal Patel <Kajal.Patel@D2L.com>
2023-09-22 15:30:56 -0400

    add tests

--- test/activities/instanceCollections/data/CollectedObject.js
+++ test/activities/instanceCollections/data/CollectedObject.js
@@ -12,7 +12,7 @@ const collected_object = {
 				'self'
 			],
 			//this doesn't exist yet
-			href: 'https://ec1ee987-fd66-4dff-af57-41aab1e6238f.activities.api.dev.brightspace.com/CollectedObject/1'
+			href: 'https://fake-tenant-id.activities.api.dev.brightspace.com/CollectedObject/1'
 		}
 	]
 };
