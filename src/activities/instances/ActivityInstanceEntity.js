d81cfb0eb41138b99f95f67ae52575b2b398e35a
Kajal Patel <Kajal.Patel@D2L.com>
2023-10-03 17:07:57 -0400

    add methods to read dates

--- src/activities/instances/ActivityInstanceEntity.js
+++ src/activities/instances/ActivityInstanceEntity.js
@@ -2,27 +2,32 @@ import { Classes } from '../../../hypermedia-constants.js';
 import { Entity } from '../../../es6/Entity.js';
 
 export class ActivityInstanceEntity extends Entity {
-	activityType(){
-		
-	}
-
-	startDate(){
+	activityType() {
 
 	}
 
-	startDateType(){
-
+	startDate() {
+		const availabilityStartDate = this._entity && this._entity.entities && this._entity.getSubEntitiesByClass(Classes.dates.startDate);
+		return availabilityStartDate && availabilityStartDate.startDate;
 	}
 
-	endDate(){
-
+	startDateType() {
+		const availabilityStartDate = this._entity && this._entity.entities && this._entity.getSubEntitiesByClass(Classes.dates.startDate);
+		return availabilityStartDate && availabilityStartDate.startDateType;
 	}
 
-	endDateType(){
-
+	endDate() {
+		const availabilityEndDate = this._entity && this._entity.entities && this._entity.getSubEntitiesByClass(Classes.dates.startDate);
+		return availabilityEndDate && availabilityEndDate.endDate;
 	}
 
-	dueDate(){
+	endDateType() {
+		const availabilityEndDate = this._entity && this._entity.entities && this._entity.getSubEntitiesByClass(Classes.dates.startDate);
+		return availabilityEndDate && availabilityEndDate.endDateType;
+	}
 
+	dueDate() {
+		const dueDate = this._entity && this._entity.entities && this._entity.getSubEntitiesByClass(Classes.dates.dueDate);
+		return dueDate && dueDate.date;
 	}
 }
