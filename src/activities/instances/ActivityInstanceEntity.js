892634f20b0eedcdb90129dd9cce24618a9b0679
Adam Kulas <Adam.Kulas@D2L.com>
2023-10-03 12:00:05 -0400

    working

---
+++ src/activities/instances/ActivityInstanceEntity.js
@@ -0,0 +1,28 @@
+import { Classes } from '../../../hypermedia-constants.js';
+import { Entity } from '../../../es6/Entity.js';
+
+export class ActivityInstanceEntity extends Entity {
+	activityType(){
+		
+	}
+
+	startDate(){
+
+	}
+
+	startDateType(){
+
+	}
+
+	endDate(){
+
+	}
+
+	endDateType(){
+
+	}
+
+	dueDate(){
+
+	}
+}
