344b0ee1f6c4426899f613e35422cca022ae7896
Venkata Ramana <venkata.ramana@d2l.com>
2024-03-01 14:10:06 +0530

    checking UT

--- test/activities/content/ContentFileEntity.test.js
+++ test/activities/content/ContentFileEntity.test.js
@@ -40,8 +40,8 @@ describe('ContentFileEntity', () => {
 		it('Equality should return true when details match', () => {
 			const fileData = {
 				title: 'Test File Title',
+				descriptionRichText: '<p>description text</p>',
 				fileHref: 'https://fake-tenant-id.files.api.proddev.d2l/my-file.file/usages/6614',
-				descriptionRichText: '<p>description text</p>'
 			};
 			expect(contentFileEntity.equals(fileData)).to.equal(true);
 		});
