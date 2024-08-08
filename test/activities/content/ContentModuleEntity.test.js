70c0e45e476672695e94254386769a6a169bde25
Juan Orjuela <Juan.Orjuela@d2l.com>
2024-08-08 11:48:54 -0500

    FED-3577 Add unit tests

--- test/activities/content/ContentModuleEntity.test.js
+++ test/activities/content/ContentModuleEntity.test.js
@@ -47,8 +47,16 @@ describe('ContentModuleEntity', () => {
 			expect(contentModuleEntity.moduleId()).to.equal('12345');
 		});
 
+		it('reads registry id', () => {
+			expect(contentModuleEntity.registryId()).to.equal('38db1f7d-7917-445d-867e-67034387744b');
+		});
+
 		it('reads generate module summary url', () => {
 			expect(contentModuleEntity.generateSummaryEndpoint()).to.equal('https://fake-tenant-id.modules.api.proddev.d2l/6613/modules/12345/summary');
+		});
+
+		it('reads lores endpoint url', () => {
+			expect(contentModuleEntity.loresEndpoint()).to.equal('https://dev-lores.fake.com/');
 		});
 
 		it('reads ai inspired', () => {
