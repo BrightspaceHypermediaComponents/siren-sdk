3d56bc602bea0ab78d2c7fd4692f4f3be5b42bf9
Anthony Wikkerink <awikkerink@desire2learn.com>
2024-08-28 10:58:41 -0400

    Add typescript jsdoc for better IDE integration

--- src/es6/Entity.js
+++ src/es6/Entity.js
@@ -2,14 +2,19 @@ import { dispose, entityFactory, updateEntity } from './EntityFactory.js';
 import { EntitySirenProperties } from './EntitySirenProperties.js';
 
 /**
+ * @typedef {import('siren-parser').Entity} ParsedEntity
+ * @typedef {import('./EntityFactory').EntityListener} EntityListener
+ */
+
+/**
  * Abstract Entity class to help create entity classes.
  */
 export class Entity extends EntitySirenProperties {
 	/**
 	 * Primes the object used by the entityFactory. Should never be called outside.
-	 * @param {Object} entity A hypermedia siren entity as defined by [the siren specification]{@link https://github.com/kevinswiber/siren}
-	 * @param {String|Function|null} token JWT Token for brightspace | a function that returns a JWT token for brightspace | null (defaults to cookie authentication in a browser)
-	 * @param {Function} listener Listener helper class
+	 * @param {ParsedEntity} entity A hypermedia siren entity as defined by [the siren specification]{@link https://github.com/kevinswiber/siren}
+	 * @param {String|Function} [token] JWT Token for brightspace | a function that returns a JWT token for brightspace | null (defaults to cookie authentication in a browser)
+	 * @param {EntityListener} listener Listener helper class
 	 */
 	constructor(entity, token, listener) {
 		super();
@@ -17,6 +22,10 @@ export class Entity extends EntitySirenProperties {
 			throw new TypeError('Cannot construct Entity instances directly');
 		}
 		this._entity = entity;
+
+		/**
+		 * @type {Map<string, EntitySirenProperties>}
+		 */
 		this._subEntities = new Map();
 		this._token = token;
 		this._listener = listener;
@@ -35,6 +44,10 @@ export class Entity extends EntitySirenProperties {
 	subEntitiesLoaded() {
 		return Promise.all(this._subEntitiesLoadStatus);
 	}
+
+	/**
+	 * @param {ParsedEntity} entity
+	 */
 	update(entity) {
 		updateEntity(this.self(), this._token, entity);
 	}
@@ -49,9 +62,11 @@ export class Entity extends EntitySirenProperties {
 	}
 	/**
 	 * Protected: Add a listener to a subentity of this entity.
-	 * @param {*} entityType A entity class that extends this class.
-	 * @param {*} href Href or Entity of the entity to be created
-	 * @param {*} onChange callback function that accepts an {entityType} to be called when subentity changes.
+	 *
+	 * @template {EntitySirenProperties} EntityType
+	 * @param {new (...args: any[]) => EntityType} entityType A entity class that extends this class.
+	 * @param {string|ParsedEntity} href Href or Entity of the entity to be created
+	 * @param {(entityType: EntityType, error: any) => void} onChange callback function that accepts an {entityType} to be called when subentity changes.
 	 */
 	_subEntity(entityType, href, onChange) {
 		if (!href) {
@@ -67,9 +82,11 @@ export class Entity extends EntitySirenProperties {
 
 	/**
 	 * Protected: Add a listener to a subentity of this entity.
-	 * @param {*} entityType A entity class that extends this class.
-	 * @param {*} source Href of the entity to be created
-	 * @param {*} onChange callback function that accepts an {entityType} to be called when subentity changes.
+	 *
+	 * @template {EntitySirenProperties} EntityType
+	 * @param {new (...args: any[]) => EntityType} entityType A entity class that extends this class.
+	 * @param {string} source Href of the entity to be created
+	 * @param {(entityType: EntityType, error: any) => void} onChange callback function that accepts an {entityType} to be called when subentity changes.
 	 */
 	_subEntityByHref(entityType, source, onChange) {
 		// Clean up if that href has already been added.
@@ -102,9 +119,11 @@ export class Entity extends EntitySirenProperties {
 
 	/**
 	 * Protected: Add a listener to a subentity of this entity.
-	 * @param {*} entityType A entity class that extends this class.
-	 * @param {*} entity Entity that has already been fetched as a sub-entity. Requires either an href or a self link
-	 * @param {*} onChange callback function that accepts an {entityType} to be called when subentity changes.
+	 *
+	 * @template {EntitySirenProperties} EntityType
+	 * @param {new (...args: any[]) => EntityType} entityType A entity class that extends this class.
+	 * @param {ParsedEntity} entity Entity that has already been fetched as a sub-entity. Requires either an href or a self link
+	 * @param {(entityType: EntityType, error: any) => void} onChange callback function that accepts an {entityType} to be called when subentity changes.
 	 */
 	_subEntityByEntity(entityType, entity, onChange) {
 		if (!entity) {
