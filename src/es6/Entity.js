fffd245f27a6e18146218811bf1bcab799af6d8c
Anthony Wikkerink <awikkerink@desire2learn.com>
2024-08-29 11:25:00 -0400

    Make listener optional

--- src/es6/Entity.js
+++ src/es6/Entity.js
@@ -14,7 +14,7 @@ export class Entity extends EntitySirenProperties {
 	 * Primes the object used by the entityFactory. Should never be called outside.
 	 * @param {ParsedEntity} entity A hypermedia siren entity as defined by [the siren specification]{@link https://github.com/kevinswiber/siren}
 	 * @param {String|Function} [token] JWT Token for brightspace | a function that returns a JWT token for brightspace | null (defaults to cookie authentication in a browser)
-	 * @param {EntityListener} listener Listener helper class
+	 * @param {EntityListener} [listener] Listener helper class
 	 */
 	constructor(entity, token, listener) {
 		super();
